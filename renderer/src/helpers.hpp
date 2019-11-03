0e09e428edb394086329f4b0d33111b609013aa4
Carl Findahl <sportleroro@gmail.com>
2019-11-03 15:33:56 +0100

    Add copy / move control Macros to helper

--- renderer/src/helpers.hpp
+++ renderer/src/helpers.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#define ULFEYE_NO_COPY(classname)         \
+    classname(const classname&) = delete; \
+    classname& operator=(const classname&) = delete
+
+#define ULFEYE_NO_MOVE(classname)    \
+    classname(classname&&) = delete; \
+    classname& operator=(classname&&) = delete
+
 namespace ulf
 {
-
 }
