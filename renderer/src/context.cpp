954a188993f5c1f13cf8027be2c955534995a553
Carl Findahl <sportleroro@gmail.com>
2019-11-03 15:23:20 +0100

    Write initial ctors and data for RenderContext

--- renderer/src/context.cpp
+++ renderer/src/context.cpp
@@ -5,3 +5,22 @@
  */
 
 #include "context.hpp"
+
+#include <doctest/doctest.h>
+
+namespace ulf
+{
+RenderContext::RenderContext() { init_instance("Vulkan Renderer"); }
+
+RenderContext::RenderContext(std::string_view appname) { init_instance(appname); }
+
+vk::Result RenderContext::init_instance(std::string_view appname) {
+    const auto appinfo = vk::ApplicationInfo(appname.data(), 1u, nullptr, 0, VK_API_VERSION_1_1);
+//    vk::createInstance()
+}
+
+/** --- TESTS --- */
+
+TEST_CASE("RenderContext - Initialization") { auto context = RenderContext{}; }
+
+}  // namespace ulf
--- renderer/src/context.hpp
+++ renderer/src/context.hpp
@@ -1,9 +1,36 @@
 #pragma once
 
+#include <array>
+#include <vector>
+
 #include <vulkan/vulkan.hpp>
 
 namespace ulf
 {
+/**
+ * @struct RenderContextSettings
+ * @brief Contains settings for creating a RenderContext
+ * @author Carl Findahl
+ * @date 03.11.2019
+ */
+struct RenderContextSettings
+{
+    /** Required instance extensions */
+    std::vector<vk::ExtensionProperties> instance_ext{};
+
+    /** Required device extensions */
+    std::vector<vk::ExtensionProperties> device_ext{};
+
+    /** Do we prefer using a discrete GPU */
+    bool prefer_discrete = true;
+};
+
+/**
+ * @class RenderContext
+ * @brief Contains the Vulkan Instance, and chosen Phyiscal + Logical Device. Bare minimum for having an application with Vulkan.
+ * @author Carl Findahl
+ * @date 03.11.2019
+ */
 class RenderContext
 {
 private:
@@ -17,6 +44,11 @@ private:
     vk::Device m_device{};
 
 public:
+    RenderContext();
+    RenderContext(std::string_view appname);
+
+private:
+    vk::Result init_instance(std::string_view appname);
 };
 
 }  // namespace ulf
