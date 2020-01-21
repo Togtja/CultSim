9cea307b55ba011e1f0ec3dd8d7d28c915caba30
Carl Findahl <sportleroro@gmail.com>
2020-01-21 13:36:50 +0100

    Change SDL from Find_package to FetchContent

--- external/sdl2.cmake
+++ external/sdl2.cmake
@@ -1,14 +1,12 @@
 # Set up sdl2, either by finding the package directly, or by fetching it from GitHub
 
-if(NOT UNIX)
-    include(FetchContent)  
-    message(STATUS "Could not find sdl2 package so we are downloading it.")
-    # Declare where to find SDL2 and what version to use
-    FetchContent_Declare(
-        sdl2_external
-        URL https://www.libsdl.org/release/SDL2-devel-2.0.10-VC.zip
-    )
+include(FetchContent)  
+message(STATUS "Could not find sdl2 package so we are downloading it.")
+# Declare where to find SDL2 and what version to use
+FetchContent_Declare(
+    sdl2_external
+    URL https://www.libsdl.org/release/SDL2-2.0.10.tar.gz
+)
 
-    # Populate it for building
-    FetchContent_MakeAvailable(sdl2_external)
-endif()
+# Populate it for building
+FetchContent_MakeAvailable(sdl2_external)
