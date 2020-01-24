0b95fe75f1f18dfd85a3fb9a27c91fdc9aa6f81a
Tomas Berger <togtja@gmail.com>
2020-01-24 14:41:19 +0100

    #13 Implement Window member functions init and display, clear, set_background_color

--- bachelor/sim/src/window.cpp
+++ bachelor/sim/src/window.cpp
@@ -1,7 +1,7 @@
 #include "window.h"
 
-#include <spdlog/spdlog.h>
 #include <glad/glad.h>
+#include <spdlog/spdlog.h>
 
 namespace cs
 {
@@ -10,27 +10,45 @@ SDL_Window* Window::get_window()
     return m_window;
 }
 
-Window::Window()
-{
-}
-
 bool Window::init(std::string name, int width, int height)
 {
-    m_window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
+    m_window = SDL_CreateWindow(name.c_str(),
+                                SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED,
+                                width,
+                                height,
+                                SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if (m_window == nullptr)
     {
         spdlog::error("failed to create window");
         return false;
     }
+
+    SDL_GLprofile profile = SDL_GL_CONTEXT_PROFILE_CORE;
+
+    // Create a double buffer context with the provided settings
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, profile);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    m_context = SDL_GL_CreateContext(m_window);
+
     return true;
 }
-void Window::clear(){
+
+void Window::clear()
+{
     glClear(GL_COLOR_BUFFER_BIT);
 }
-void Window::display(){
 
+void Window::display()
+{
+    SDL_GL_SwapWindow(m_window);
 }
-void Window::set_background_color(){
-    //glClearColor();
+
+void Window::set_background_color(glm::vec3 color)
+{
+    glClearColor(color.r, color.g, color.b, 0.f);
 }
 } // namespace cs
--- bachelor/sim/src/window.h
+++ bachelor/sim/src/window.h
@@ -3,20 +3,35 @@
 #include <string>
 
 #include <SDL.h>
+#include <glm/vec3.hpp>
+
 namespace cs
 {
 class Window
 {
 private:
     SDL_Window* m_window = nullptr;
+    SDL_GLContext m_context;
 
 public:
-    Window();
     SDL_Window* get_window();
+
+    /**
+     * init will initialize a Window with the provided title at the provided size
+     *
+     * @param name is the title of the window
+     * @param width requested width (and resolution) of the window
+     * @param length requested length (and resolution) of the window
+     * @return A pointer to the newly created SDL Window, or NULL if it failed
+     */
     bool init(std::string name = "CultSim", int width = 1280, int height = 720);
+
     void clear();
+
     void display();
-    void set_background_color();
+
+    void set_background_color(glm::vec3 color);
+
 private:
 };
 } // namespace cs
