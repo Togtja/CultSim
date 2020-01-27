6ee0af1555c62767e3db85ec97b06b5d3623732d
Carl Findahl <sportleroro@gmail.com>
2020-01-27 17:33:29 +0100

    #22 Revert GLM to always cloen

--- external/glm.cmake
+++ external/glm.cmake
@@ -1,23 +1,15 @@
-# Set up GLM, either by finding the package directly, or by fetching it from GitHub
+# Set up GLM by fetching it from GitHub
 find_package(glm 0.9)
 
-if(spdlog_FOUND)
-    set_target_properties(
-        spdlog::spdlog
-        PROPERTIES
-        IMPORTED_GLOBAL TRUE
-    )
-else()
-    message(STATUS "Could not find GLM package so we are getting it from GitHub instead.")
+message(STATUS "Getting GLM from Github.")
 
-    # Declare where to find GLM and what version to use
-    FetchContent_Declare(
-        glm_external
-        GIT_REPOSITORY https://github.com/g-truc/glm.git
-        GIT_TAG 0.9.9.7
-        GIT_PROGRESS TRUE
-    )
+# Declare where to find GLM and what version to use
+FetchContent_Declare(
+    glm_external
+    GIT_REPOSITORY https://github.com/g-truc/glm.git
+    GIT_TAG 0.9.9.7
+    GIT_PROGRESS TRUE
+)
 
-    # Populate it for building
-    FetchContent_MakeAvailable(glm_external)
-endif()
+# Populate it for building
+FetchContent_MakeAvailable(glm_external)
