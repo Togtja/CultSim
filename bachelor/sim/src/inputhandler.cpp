5e320e270117f24f33462e29c6b3402974ca33f5
Tomas Berger <togtja@gmail.com>
2020-01-30 14:41:33 +0100

    #15 Create remove_context function for context handler

--- bachelor/sim/src/inputhandler.cpp
+++ bachelor/sim/src/inputhandler.cpp
@@ -63,6 +63,20 @@ void ContextHandler::add_context(KeyContext context)
     }
 }
 
+void ContextHandler::remove_context(KeyContext context)
+{
+    auto context_it = std::find(std::begin(m_active_stack), std::end(m_active_stack), context);
+    /** If it is not the end then we have it */
+    if (context_it != std::end(m_active_stack))
+    {
+        m_active_stack.erase(context_it);
+    }
+    else
+    {
+        spdlog::debug("trying to remove context id {} and it is not here from before", context);
+    }
+}
+
 void ContextHandler::bind_key(KeyContext context, const SDL_Scancode event, const std::function<void()> function)
 {
     m_input_map[context].bind_key(event, function);
