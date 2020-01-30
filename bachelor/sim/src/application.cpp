45ebd59e09808bc6ad5afc3ea6524daffc472c9b
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-01-30 13:44:28 +0100

    Close #4 Remove ImGui test code from main application

--- bachelor/sim/src/application.cpp
+++ bachelor/sim/src/application.cpp
@@ -39,40 +39,7 @@ void Application::run(const std::vector<char*>& args)
         ImGui_ImplSDL2_NewFrame(m_window.get());
         ImGui::NewFrame();
         {
-            static int counter = 0;
-            // get the window size as a base for calculating widgets geometry
-            int sdl_width = 0, sdl_height = 0, controls_width = 0;
-            SDL_GetWindowSize(m_window.get(), &sdl_width, &sdl_height);
-            controls_width = sdl_width;
-            // make controls widget width to be 1/3 of the main window width
-            if ((controls_width /= 3) < 300)
-            {
-                controls_width = 300;
-            }
-
-            // position the controls widget in the top-right corner with some margin
-            ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Always);
-            // here we set the calculated width and also make the height to be
-            // be the height of the main window also with some margin
-            ImGui::SetNextWindowSize(ImVec2(static_cast<float>(controls_width), static_cast<float>(sdl_height - 20)),
-                                     ImGuiCond_Always);
-            // create a window and append into it
-            ImGui::Begin("Controls", NULL, ImGuiWindowFlags_NoResize);
-
-            ImGui::Dummy(ImVec2(0.0f, 1.0f));
-            ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), "Platform");
-            ImGui::Text("%s", SDL_GetPlatform());
-            ImGui::Text("CPU cores: %d", SDL_GetCPUCount());
-            ImGui::Text("RAM: %.2f GB", SDL_GetSystemRAM() / 1024.0f);
-
-            // buttons and most other widgets return true when clicked/edited/activated
-            if (ImGui::Button("Counter button"))
-            {
-                std::cout << "counter button clicked\n";
-                counter++;
-            }
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::Begin("TEST", NULL, ImGuiWindowFlags_NoResize);
 
             ImGui::End();
         }
