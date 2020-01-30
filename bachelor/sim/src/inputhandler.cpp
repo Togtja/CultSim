443fa96cbd0abe4a9d81b0ba3a5f4f37786d59c7
Tomas Berger <togtja@gmail.com>
2020-01-30 14:39:30 +0100

    #15 Create bind_key function for input handler

--- bachelor/sim/src/inputhandler.cpp
+++ bachelor/sim/src/inputhandler.cpp
@@ -13,6 +13,18 @@ InputHandler::InputHandler(KeyContext type)
     m_context_type = type;
 }
 
+void InputHandler::bind_key(const SDL_Scancode event, const std::function<void()> function)
+{
+    auto key_it = m_key_binding.find(event);
+    if (key_it == m_key_binding.end())
+    {
+        m_key_binding.emplace(event, function);
+    }
+    else
+    {
+        spdlog::debug("this context (id: {}) already has a binding for this event ({})", m_context_type, event);
+    }
+}
 InputHandler::~InputHandler()
 {
 }
