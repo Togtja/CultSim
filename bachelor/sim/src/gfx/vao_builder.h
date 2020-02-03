f07ca4f16f41462ce35470de66618ba07165b389
Carl Findahl <sportleroro@gmail.com>
2020-02-03 13:46:27 +0100

    awd

---
+++ bachelor/sim/src/gfx/vao_builder.h
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace cs
+{
+namespace gfx
+{
+class VaoBuilder
+{
+private:
+    unsigned m_vao{};
+
+public:
+    VaoBuilder();
+    VaoBuilder(const VaoBuilder&) = delete;
+    VaoBuilder(VaoBuilder&&)      = delete;
+    VaoBuilder& operator=(const VaoBuilder&) = delete;
+    VaoBuilder& operator=(VaoBuilder&&) = delete;
+    ~VaoBuilder();
+
+    VaoBuilder& attribute(unsigned index, int size, unsigned type, unsigned offset, bool normalized = false);
+
+    VaoBuilder& iattribute(unsigned index, int size, unsigned type, unsigned offset);
+
+    VaoBuilder& enable(unsigned index);
+
+    VaoBuilder& vbo(unsigned vbo, unsigned binding);
+
+    VaoBuilder& ebo(unsigned ebo);
+
+    VaoBuilder& divisor(unsigned binding, unsigned divisor);
+
+    unsigned build() const;
+};
+} // namespace gfx
+
+} // namespace cs
