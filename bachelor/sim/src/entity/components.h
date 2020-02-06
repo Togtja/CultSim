939cd331cae19af5a4191a0ee99521185dc868cd
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-06 13:55:48 +0100

    #36 Fix spelling mistake

--- bachelor/sim/src/entity/components.h
+++ bachelor/sim/src/entity/components.h
@@ -35,13 +35,13 @@ struct Emotion
     /*In percentages from -100 - 100 */
 
     /*-100=Depressed - 100= Ecstatic*/
-    float happiness;
+    float happiness{};
 
     /*-100=Livid - 100 = tranquil*/
-    float calm;
+    float calm{};
 
     /*-100=Unmotivated - 100 = Very motivated*/
-    float motivation;
+    float motivation{};
 };
 
 struct Status
@@ -49,13 +49,13 @@ struct Status
     /*In percentages from 0 - 100 */
 
     /*0=Dead - 100 = Full health*/
-    float health;
+    float health = 100;
 
     /*0 = No energy - 100 = Full energy*/
-    float engergy;
+    float energy = 100;
 
     /*0 = Starving - 100 = Well fed*/
-    float nourishment;
+    float nourishment = 100;
 };
 
 struct Attributes
@@ -63,21 +63,71 @@ struct Attributes
     /*In percentages from -100 - 100 */
 
     /*-100 = repulsive - 100 = enticing  */
-    float attractiveness;
+    float attractiveness{};
 
     /*-100 = terrifying - 100 = soothing*/
-    float soothing;
+    float soothing{};
 };
 
-struct Senses
+struct Sense
 {
+    enum ESenses
+    {
+        Vision = 1 << 0,
+        Smell  = 1 << 1,
+        Sound  = 1 << 2,
+        None   = 1 << 4,
 
-    bool blinded;
-    float vision_radius;
-    float fov;
+    };
 
-    bool senses_impaired;
-    float sense_radius;
+    ESenses senses = None;
+
+    float vision_radius{};
+    float fov{};
+
+    float smell_radius{};
+
+    float sound_radius{};
+};
+
+struct Reproduction
+{
+    bool can_reproduce= false;
+
+    enum ESex
+    {
+        Male   = true,
+        Female = false
+    };
+
+    ESex sex = Male;
+
+    uint16_t number_of_children{};
+};
+
+struct Produce
+{
+    enum EProduce
+    {
+        Food,
+        Silk,
+        Hemp,
+        Drugs,
+        Milk
+    };
+
+    std::vector<EProduce> produces{};
+};
+
+struct Drop_Items
+{
+    enum EDeathDrops
+    {
+        Meat,
+        Wood
+    };
+
+    std::vector<EDeathDrops> deathdrops{};
 };
 
 struct AI
