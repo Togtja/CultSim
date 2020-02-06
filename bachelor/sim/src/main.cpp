051809ff94f9edec57229f6e5b22613af74ed29f
Carl Findahl <sportleroro@gmail.com>
2020-02-06 14:30:04 +0100

    #16 Add default doctest options to main

--- bachelor/sim/src/main.cpp
+++ bachelor/sim/src/main.cpp
@@ -9,14 +9,20 @@ int main(int argc, char* argv[])
 {
     const auto args = std::vector(argv, argv + argc);
 
-    doctest::Context context(args.size(), args.data());
-    int res = context.run();
+    /** Set up testing context and set some default options */
+    doctest::Context context{};
+    context.setOption("abort-after", 5);
+    context.setOption("no-breaks", true);
+    context.applyCommandLine(args.size(), args.data());
 
+    /** Check if we still want to run the program */
+    int res = context.run();
     if (context.shouldExit())
     {
         return res;
     }
 
+    /** Run the program */
     cs::Application cultsim{};
     cultsim.run(args);
 
