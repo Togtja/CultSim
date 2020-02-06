84595ca0841fdc0e1d5118970478129973c70be4
Tomas Berger <togtja@gmail.com>
2020-02-06 15:51:44 +0100

    #31 Add random path_finding function to .h file

--- bachelor/sim/src/entity/ai.h
+++ bachelor/sim/src/entity/ai.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "entity/system.h"
+
+#include <random>
+
 #include <glm/vec2.hpp>
 
 namespace cs::system
@@ -11,7 +14,15 @@ public:
     using ISystem::ISystem;
     bool is_visible(glm::vec2 pos, glm::vec2 pos2, float rad);
     bool is_colliding();
+    glm::vec2 path_finding()
+    {
+        static std::random_device rd{};
+        static std::mt19937 gen(rd());
+        std::uniform_int_distribution dist(-360, 360);
+        return glm::vec2(dist(gen), dist(gen));
+    }
     void update(float dt) override;
+
 private:
     bool close_enough(glm::vec2 pos, glm::vec2 pos2, float treshold);
 };
