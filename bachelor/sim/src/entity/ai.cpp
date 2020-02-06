8f2b9b8a324ccfb397c2f2e275aaeb7afeafb253
Carl Findahl <sportleroro@gmail.com>
2020-02-06 15:58:35 +0100

    #31 Clean up and move implementation to cpp file

--- bachelor/sim/src/entity/ai.cpp
+++ bachelor/sim/src/entity/ai.cpp
@@ -10,16 +10,21 @@ bool AI::is_visible(glm::vec2 pos, glm::vec2 pos2, float rad)
 {
     float x = pos.x - pos2.x;
     float y = pos.y - pos2.y;
-    if (x * x + y * y <= rad * rad)
-    {
-        return true;
-    }
-    return false;
+    return x * x + y * y <= rad * rad;
 }
 bool AI::is_colliding()
 {
     return false;
 }
+
+glm::vec2 AI::path_finding()
+{
+    static std::random_device rd{};
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution dist(-360, 360);
+    return glm::vec2(dist(gen), dist(gen));
+}
+
 void AI::update(float dt)
 {
     auto view = m_registry.view<component::Position, component::Movement, component::Sprite, component::Vision>();
@@ -62,4 +67,5 @@ bool AI::close_enough(glm::vec2 pos, glm::vec2 pos2, float treshold)
     glm::bvec2 boolvec = glm::epsilonEqual(pos, pos2, glm::vec2(treshold));
     return boolvec.x && boolvec.y;
 }
+
 } // namespace cs::system
--- bachelor/sim/src/entity/ai.h
+++ bachelor/sim/src/entity/ai.h
@@ -9,18 +9,15 @@ namespace cs::system
 {
 class AI : public ISystem
 {
-private:
 public:
     using ISystem::ISystem;
+
     bool is_visible(glm::vec2 pos, glm::vec2 pos2, float rad);
+
     bool is_colliding();
-    glm::vec2 path_finding()
-    {
-        static std::random_device rd{};
-        static std::mt19937 gen(rd());
-        std::uniform_int_distribution dist(-360, 360);
-        return glm::vec2(dist(gen), dist(gen));
-    }
+
+    glm::vec2 path_finding();
+
     void update(float dt) override;
 
 private:
