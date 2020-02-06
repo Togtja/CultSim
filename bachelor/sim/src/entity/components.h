d7096e1441252f987233b4e826120702d5fd5d3f
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-06 13:00:00 +0100

    #36 Add some more components

--- bachelor/sim/src/entity/components.h
+++ bachelor/sim/src/entity/components.h
@@ -29,4 +29,44 @@ struct Animation
     float time_per_frame = 1.f / 4.f;
     float current_progress{};
 };
-}
+
+struct Emotion
+{
+    /*In percentages from -100 - 100 */
+
+    /*-100=Depressed - 100= Ecstatic*/
+    float happiness;
+
+    /*-100=Livid - 100 = tranquil*/
+    float calm;
+
+    /*-100=Unmotivated - 100 = Very motivated*/
+    float motivation;
+};
+
+struct Status
+{
+    /*In percentages from 0 - 100 */
+
+    /*0=Dead - 100 = Full health*/
+    float health;
+
+    /*0 = No energy - 100 = Full energy*/
+    float engergy;
+
+    /*0 = Starving - 100 = Well fed*/
+    float nourishment;
+};
+
+struct Attributes
+{
+    /*In percentages from -100 - 100 */
+
+    /*-100 = repulsive - 100 = enticing  */
+    float attractiveness;
+
+    /*-100 = terrifying - 100 = soothing*/
+    float soothing;
+};
+
+} // namespace cs::component
