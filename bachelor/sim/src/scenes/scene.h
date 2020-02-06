e4c8fce5c2b79b2146d5b88aa42bea2b176e0a23
Carl Findahl <sportleroro@gmail.com>
2020-02-06 14:10:37 +0100

    #37 Document scene and scene manager

--- bachelor/sim/src/scenes/scene.h
+++ bachelor/sim/src/scenes/scene.h
@@ -12,16 +12,29 @@ public:
     IScene& operator=(IScene&& other) noexcept = default;
     virtual ~IScene() noexcept                 = default;
 
+    /**
+     * Called when the scene is entered
+     */
     virtual void on_enter() = 0;
 
+    /**
+     * Called when the scene is exited
+     */
     virtual void on_exit() = 0;
 
+    /**
+     * Called every frame and is meant for logic updates
+     *
+     * @param dt Delta time since previous call to update
+     * @return true if this scene allow other scenes to update after it
+     */
     virtual bool update(float dt) = 0;
 
-    /*
-    * draw is responsible for drawing the scene
-    * @return if it returns true, scenes below this one still draw
-    */
+    /**
+     * Draw is responsible for drawing the scene
+     *
+     * @return if it returns true, scenes below this one still draw
+     */
     virtual bool draw() = 0;
 };
 } // namespace cs
