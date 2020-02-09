e7c8260bb3ca40d8d7b59acaf580884213620967
Carl Findahl <sportleroro@gmail.com>
2020-02-09 13:10:57 +0100

    #44 Reduce axis gizmo size

--- bachelor/sim/src/application.cpp
+++ bachelor/sim/src/application.cpp
@@ -74,9 +74,9 @@ void Application::draw()
     auto& r = gfx::get_renderer();
     r.sprite().display();
 
-    r.debug().draw_line({-1000.f, 0.f, 0.f}, {1000.f, 0.f, 0.f}, {1.f, 0.f, 0.f});
-    r.debug().draw_line({0.f, -1000.f, 0.f}, {0.f, 1000.f, 0.f}, {0.f, 1.f, 0.f});
-    r.debug().draw_line({0.f, 0.f, -1000.f}, {0.f, 0.f, 1000.f}, {0.f, 0.f, 1.f});
+    r.debug().draw_line({-100.f, 0.f, 0.f}, {100.f, 0.f, 0.f}, {1.f, 0.f, 0.f});
+    r.debug().draw_line({0.f, -100.f, 0.f}, {0.f, 100.f, 0.f}, {0.f, 1.f, 0.f});
+    r.debug().draw_line({0.f, 0.f, -100.f}, {0.f, 0.f, 100.f}, {0.f, 0.f, 1.f});
 
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
