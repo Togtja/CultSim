afb3e879ad8f9e5185fd14da8ef63c9b50f97ff6
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-10 10:31:30 +0100

    #45 Add update function to need.cpp

--- bachelor/sim/src/entity/systems/need.cpp
+++ bachelor/sim/src/entity/systems/need.cpp
@@ -1,1 +1,13 @@
 #include "need.h"
+#include "entity/components/components.h"
+
+namespace cs::system
+{
+void Need::update(float dt)
+{
+    auto view = m_registry.view<component::Position, component::Movement>();
+    view.each([dt](component::Needs& needs) {
+        for (auto need : needs.needs ) {
+          need.status -= need.decay_rate;
+        }
+} // namespace cs::system
