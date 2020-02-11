0fb00a729c66f2263d376784a632aa1d8b394e78
Carl Findahl <sportleroro@gmail.com>
2020-02-11 16:30:13 +0100

    #51 Update movement and rendering to visualize data

--- bachelor/sim/src/entity/systems/rendering.cpp
+++ bachelor/sim/src/entity/systems/rendering.cpp
@@ -1,10 +1,23 @@
 #include "rendering.h"
 #include "entity/components/components.h"
 
+#include "gfx/ImGUI/imgui.h"
+#include <glm/gtc/type_ptr.hpp>
+
 namespace cs::system
 {
 void Rendering::update(float dt)
 {
+    static glm::vec3 noseecolor{1.f, 0.f, 0.f};
+    static glm::vec3 seecolor{0.f, 1.f, 0.f};
+
+    ImGui::ColorPicker3("No See Color", glm::value_ptr(noseecolor));
+    ImGui::ColorPicker3("See Color", glm::value_ptr(seecolor));
+
+    m_registry.group<component::Sprite>(entt::get<component::Vision>).each([](component::Sprite& spr, const component::Vision& vis) {
+       spr.color = vis.seen.empty() ? noseecolor : seecolor;
+    });
+
     auto pos_sprite_view = m_registry.view<component::Position, component::Sprite>();
     pos_sprite_view.each([this](const component::Position& pos, const component::Sprite& sprite) {
         m_renderer.sprite().draw(pos.position, sprite.color, sprite.texture);
