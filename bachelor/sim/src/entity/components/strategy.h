212c5c35287760d46c9046914256db2b100d1e7c
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-12 09:29:36 +0100

    #46 Add name and == operator to strategies for purpose of comparison

--- bachelor/sim/src/entity/components/strategy.h
+++ bachelor/sim/src/entity/components/strategy.h
@@ -7,9 +7,16 @@ namespace cs::ai
 {
 struct Strategy
 {
+    std::string name;
+
     float desirability{};
 
     std::vector<std::string> tags{};
+
+    bool operator==(Strategy const& strategy2)
+    {
+        return name == strategy2.name
+    }
 
     // TODO: Actual functionality
 };
