3367737f58c15fc41915ea7bb1f66911ea716332
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-12 14:29:14 +0100

    #46 Add skeleton code and temp debug log to update loop for action.cpp

--- bachelor/sim/src/entity/systems/action.cpp
+++ bachelor/sim/src/entity/systems/action.cpp
@@ -1,9 +1,26 @@
 
 #include "action.h"
-
+#include "..\components\action.h"
+#include "entity/components/components.h"
 namespace cs::system
 {
 void Action::update(float dt)
 {
+    auto view = m_registry.view<component::Strategies>();
+    view.each([this, dt](component::Strategies& strategies) {
+        if (!strategies.staged_strategies.empty())
+        {
+            for (auto strategy : strategies.staged_strategies)
+            {
+                for (auto action : strategy.actions)
+                {
+                    action.run();
+                    // Let run while the action is not finished / Aborted
+                    strategies.staged_strategies.clear();
+                    break;
+                }
+            }
+        }
+    });
 }
 } // namespace cs::system
