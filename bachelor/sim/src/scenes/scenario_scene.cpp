067dbeb8157c0423d958d7be5f808d1011bb16db
Tomas Berger <togtja@gmail.com>
2020-02-12 13:23:07 +0100

    #51 Add increased speed and spawn less

--- bachelor/sim/src/scenes/scenario_scene.cpp
+++ bachelor/sim/src/scenes/scenario_scene.cpp
@@ -31,7 +31,7 @@ void ScenarioScene::on_enter()
         glm::vec2 pos(i * 15, 0);
 
         m_registry.assign<component::Position>(agent, glm::vec3(pos, 0), glm::vec3(0, 0, 0));
-        m_registry.assign<component::Movement>(agent, glm::vec2(0.f, 0.f), glm::normalize(glm::vec2(1.f, 1.f)), 25.f);
+        m_registry.assign<component::Movement>(agent, glm::vec2(0.f, 0.f), glm::normalize(glm::vec2(1.f, 1.f)), 100.f);
         m_registry.assign<component::Sprite>(agent, tex, glm::vec3(1.f, 0.f, 0.f));
         m_registry.assign<component::Vision>(agent, std::vector<entt::entity>{}, 40.f, static_cast<uint8_t>(0));
         m_registry.assign<component::Needs>(agent, std::vector<ai::Need>({need}), std::vector<ai::Need>({}));
