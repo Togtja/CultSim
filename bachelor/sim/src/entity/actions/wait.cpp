39920272e556287191f71521c5a2243d7773f81f
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-13 14:46:45 +0100

    #46 Add files for wait action

---
+++ bachelor/sim/src/entity/actions/wait.cpp
@@ -0,0 +1,5 @@
+#include "wait.h"
+
+namespace cs::action
+{
+}
---
+++ bachelor/sim/src/entity/actions/wait.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "action.h"
+
+namespace cs::action
+{
+class Wait : public IAction
+{
+public:
+private:
+    using IAcction::IAction;
+};
+} // namespace cs::action
