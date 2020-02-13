ac06cbe8fbdd151900f050d04634727d8ca08ad5
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-13 14:44:21 +0100

    #46 Refactor action.cpp to have time variables as all actions are now timed actions

--- bachelor/sim/src/entity/actions/action.h
+++ bachelor/sim/src/entity/actions/action.h
@@ -11,6 +11,8 @@ class IAction
 {
 protected:
     std::vector<Requirement> m_requirements{};
+    float m_time_to_complete{};
+    float m_time_left{};
 
     virtual void success() = 0;
     virtual void failure() = 0;
@@ -18,7 +20,11 @@ protected:
 public:
     std::string m_name{};
 
-    IAction(std::string name, std::vector<Requirement> requirements) : m_name(name), m_requirements(requirements){};
+    IAction(std::string name, float time_to_complete, float time_left, std::vector<Requirement> requirements) :
+        m_name(name),
+        m_time_to_complete(time_to_complete),
+        m_time_left(time_left),
+        m_requirements(requirements){};
 
     virtual void update(float dt) = 0;
 };
