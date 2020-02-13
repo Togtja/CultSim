65b234f1f181af387e659d86d1e8ae9ad942f9a0
Carl Findahl <sportleroro@gmail.com>
2020-02-13 09:50:18 +0100

    #38 Fix depth testing and sprite blending

--- bachelor/sim/src/application.cpp
+++ bachelor/sim/src/application.cpp
@@ -118,9 +118,6 @@ bool Application::init_gl()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
 #ifndef NDEBUG
     gfx::create_debug_callback();
 #endif
