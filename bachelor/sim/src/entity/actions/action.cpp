7df07bf641dcf7686b68814111377cb46d09a037
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-13 13:55:08 +0100

    #46 Made action into an interface

--- bachelor/sim/src/entity/actions/action.cpp
+++
@@ -1,5 +0,0 @@
-#include "action.h"
-
-namespace cs::action
-{
-}
--- bachelor/sim/src/entity/actions/action.h
+++ bachelor/sim/src/entity/actions/action.h
@@ -1,8 +1,25 @@
 #pragma once
 
+#include "requirement.h"
+
+#include <string>
+#include <vector>
+
 namespace cs::action
 {
-class Action
+class IAction
 {
+protected:
+    std::vector<Requirement> m_requirements{};
+
+    virtual void success() = 0;
+    virtual void failure() = 0;
+
+public:
+    std::string m_name{};
+
+    IAction(std::string name, std::vector<Requirement> requirements) : m_name(name), m_requirements(requirements){};
+
+    virtual void update(float dt) = 0;
 };
 } // namespace cs::action
