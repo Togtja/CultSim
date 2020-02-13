3eeba0648f57a67716286e6de90d449915b76641
Carl Findahl <sportleroro@gmail.com>
2020-02-13 15:01:27 +0100

    #34 Add some initial preferences

--- bachelor/sim/src/preferences.cpp
+++ bachelor/sim/src/preferences.cpp
@@ -1,5 +1,10 @@
 #include "preferences.h"
 
+#include "gfx/ImGUI/imgui.h"
+
 namespace cs
 {
+PreferenceManager::PreferenceManager(std::string_view from_file)
+{
 }
+} // namespace cs
--- bachelor/sim/src/preferences.h
+++ bachelor/sim/src/preferences.h
@@ -1,6 +1,44 @@
 #pragma once
 
+#include "robin_hood.h"
+
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+#include <glm/vec2.hpp>
+
 namespace cs
 {
+/**
+ * Defines a preference of some kind that can hold a value of supported preference types
+ */
+template<typename T>
+struct Preference
+{
+    std::string name{};
+    std::string description{};
+    std::decay_t<T> value{};
+};
 
-}
+class PreferenceManager
+{
+private:
+    /** Window resolution */
+    Preference<glm::ivec2> m_resolution{"Resolution", "The resolution of your game window", {1280, 720}};
+
+    Preference<bool> m_fullscreen{"Fullscreen", "Should the window be fullscreen or windowed", false};
+
+    /** Localization language */
+    Preference<std::string> m_language{"Language", "Application language", "en"};
+
+    /** General purpose storage for saved variables */
+    robin_hood::unordered_map<std::string, std::string> m_saved_variables{};
+
+public:
+    PreferenceManager() = default;
+    explicit PreferenceManager(std::string_view from_file);
+};
+} // namespace cs
