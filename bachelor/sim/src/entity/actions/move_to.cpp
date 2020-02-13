f9698e53a057df2d847fcf1901ef3b141d6bbb7a
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-13 15:25:46 +0100

    #46 Add placeholder functions to move_to

--- bachelor/sim/src/entity/actions/move_to.cpp
+++ bachelor/sim/src/entity/actions/move_to.cpp
@@ -2,11 +2,17 @@
 
 namespace cs::action
 {
+void MoveTo::success()
+{
+    // We reached our goal, remove us from the stack
+}
 void MoveTo::failure()
 {
+    // We failed to reach our goal, recalculate goals
 }
 
 void MoveTo::update(float dt)
 {
+    // Set movement.desired_pos to be equal to m_pos
 }
 } // namespace cs::action
