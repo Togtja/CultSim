63e3969a95a1e12a4b1497dc69761e02ac4a32c3
Carl Findahl <sportleroro@gmail.com>
2020-02-14 10:49:48 +0100

    #54 Add initial random number generation class

---
+++ bachelor/sim/src/random_engine.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <algorithm>
+#include <random>
+#include <type_traits>
+
+namespace cs
+{
+/**
+ * Utility class for providing random number generation
+ */
+class RandomEngine
+{
+private:
+    std::random_device m_seeder;
+
+    std::mt19937 m_random_generator;
+
+public:
+    RandomEngine() : m_random_generator(m_seeder())
+    {
+    }
+
+    RandomEngine(const RandomEngine&) = delete;
+    RandomEngine& operator=(const RandomEngine&) = delete;
+
+    /**
+     * Create a uniform randomly distributed number in the given range
+     *
+     * @param min The minimum value, inclusive
+     * @param max The maximum value, inclusive
+     */
+    template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+    std::decay_t<T> uniform(T min, T max)
+    {
+        if constexpr (std::is_integral_v<T>)
+        {
+            std::uniform_int_distribution<int> distribution(min, max);
+            return distribution(m_random_generator);
+        }
+        else
+        {
+            std::uniform_real_distribution<double> distribution(min, max);
+            return distribution(m_random_generator);
+        }
+    }
+
+    /**
+     * Create a normal distribution around [mean] with given standard [deviation]
+     *
+     * @param mean The mean value
+     * @param deviation The standard deviation around the mean
+     */
+    double normal(double mean, double deviation = 5.0);
+
+    /**
+     * Return true or false with given probability
+     *
+     * @param probability The chance of returning true in range 0 - 1
+     */
+    bool trigger(double probability);
+
+    /**
+     * Simulate rolling a dice
+     *
+     * @param sides The number of sides on the dice
+     * @param rolls The number of rolls to perform
+     * @return The sum of all dice rolls
+     */
+    int roll(int sides, int rolls);
+
+    /**
+     * Pick a random element from the given container that support subscripting
+     *
+     * @param container The container to pick from
+     */
+    template<typename T>
+    decltype(auto) pick(T& container)
+    {
+        return container[uniform(0, container.size() - 1)];
+    }
+
+    /**
+     * Pick a random element from the given container that support subscripting
+     *
+     * @param container The container to pick from
+     */
+    template<typename T>
+    decltype(auto) pick(const T& container) const
+    {
+        return container[uniform(0, container.size() - 1)];
+    }
+};
+} // namespace cs
