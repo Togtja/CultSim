b3a202a085609df244c43104c66589b24295e9a3
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-14 10:08:32 +0100

    #46 Remove no longer relevant code from update()

--- bachelor/sim/src/entity/systems/action.cpp
+++ bachelor/sim/src/entity/systems/action.cpp
@@ -5,21 +5,6 @@ namespace cs::system
 {
 void Action::update(float dt)
 {
-    auto view = m_registry.view<component::Strategies>();
-    view.each([this, dt](component::Strategies& strategies) {
-        if (!strategies.staged_strategies.empty())
-        {
-            for (const auto& strategy : strategies.staged_strategies)
-            {
-                for (auto action : strategy.actions)
-                {
-                    action.run();
-                    // Let run while the action is not finished / Aborted
-                    strategies.staged_strategies.clear();
-                    break;
-                }
-            }
-        }
-    });
+    // Todo: Remake this properly
 }
 } // namespace cs::system
