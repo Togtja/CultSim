b9e3e69958201c6b62cf9a2e2e5afebb1b07c3e1
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-17 09:45:13 +0100

    #46 Add Inheritance for requirements in order to allow templates in a vector

---
+++ bachelor/sim/src/entity/actions/interface_requirement.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <functional>
+#include <string>
+
+namespace cs::action
+{
+struct IRequirement
+{
+    std::string name{};
+    bool predicate = false;
+    std::function<bool(void)> init{};
+
+
+    Requirement(std::string p_name, bool p_predicate, std::function<bool(void)> p_init, ) :
+        name(p_name),
+        predicate(p_predicate),
+        init(p_init){};
+};
+} // namespace cs::action
