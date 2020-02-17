35a6a6f4dea6787d8a443e3448c6136866315ddb
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-17 09:58:09 +0100

    #46 Update and fix requirements and Irequirements to contain destructor and to take proper constructor arguments

--- bachelor/sim/src/entity/actions/interface_requirement.h
+++ bachelor/sim/src/entity/actions/interface_requirement.h
@@ -11,7 +11,7 @@ struct IRequirement
     std::function<bool(void)> init{};
 
 
-    Requirement(std::string p_name, bool p_predicate, std::function<bool(void)> p_init, ) :
+    IRequirement(std::string p_name, bool p_predicate, std::function<bool(void)> p_init) :
         name(p_name),
         predicate(p_predicate),
         init(p_init){};
--- bachelor/sim/src/entity/actions/requirement.h
+++ bachelor/sim/src/entity/actions/requirement.h
@@ -11,16 +11,23 @@ namespace cs::action
 template<typename T>
 struct Requirement : public IRequirement
 {
-
     entt::dispatcher& dispatcher{};
+    std::function<void(const T&)> event_listener{};
 
-    Requirement(std::string p_name, bool p_predicate, std::function<bool(void)> p_init, entt::dispatcher& p_dispatcher, std::function<void(const T&)> p_event_listener) :
-        IRequirement(p_name,p_predicate,p_init),
+    Requirement(std::string p_name,
+                bool p_predicate,
+                std::function<bool(void)> p_init,
+                entt::dispatcher& p_dispatcher,
+                std::function<void(const T&)> p_event_listener) :
+        IRequirement(p_name, p_predicate, p_init),
         dispatcher(p_dispatcher),
         event_listener(p_event_listener)
     {
         dispatcher.sink<T>().connect<&Requirement::event_listener>(this);
     }
-    void std::function<void(const T&)> event_listener{};
+    ~Requirement()
+    {
+        dispatcher.sink<T>().disconnect<&Requirement::event_listener>(this);
+    }
 };
 } // namespace cs::action
