a28496275e19b27514ad059c9cc00e222cfbc058
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-17 09:45:59 +0100

    #46 Add template functionality and inheritance to requirements to generalise event listening system

--- bachelor/sim/src/entity/actions/requirement.h
+++ bachelor/sim/src/entity/actions/requirement.h
@@ -1,13 +1,26 @@
 #pragma once
+#include "interface_requirement.h"
+
 #include <functional>
 #include <string>
 
+#include "entt/signal/dispatcher.hpp"
+
 namespace cs::action
 {
-struct Requirement
+template<typename T>
+struct Requirement : public IRequirement
 {
-    std::string name;
-    std::function<bool(void)> init{};
-    std::function<bool(void)> predicate{};
+
+    entt::dispatcher& dispatcher{};
+
+    Requirement(std::string p_name, bool p_predicate, std::function<bool(void)> p_init, entt::dispatcher& p_dispatcher, std::function<void(const T&)> p_event_listener) :
+        IRequirement(p_name,p_predicate,p_init),
+        dispatcher(p_dispatcher),
+        event_listener(p_event_listener)
+    {
+        dispatcher.sink<T>().connect<&Requirement::event_listener>(this);
+    }
+    void std::function<void(const T&)> event_listener{};
 };
 } // namespace cs::action
