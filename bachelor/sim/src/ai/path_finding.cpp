184d3b8c0ef2f98731db37e613d1ebc365c230d5
Tomas Berger <togtja@gmail.com>
2020-02-17 11:02:16 +0100

    #55 Add A* pathfinding (not working)

--- bachelor/sim/src/ai/path_finding.cpp
+++ bachelor/sim/src/ai/path_finding.cpp
@@ -1,4 +1,7 @@
 #include "path_finding.h"
+
+#include <spdlog/spdlog.h>
+
 static robin_hood::unordered_flat_map<PathGrid, PathGrid> a_star_grid{};
 static robin_hood::unordered_flat_map<PathGrid, float> a_star_cost{};
 
@@ -21,4 +24,58 @@ void reconstruct_path(PathGrid start, PathGrid goal, std::vector<glm::vec3>& pos
         pos.emplace_back(glm::vec3(curr.x * 32, curr.y * 32, 0));
         curr = a_star_grid[curr];
     } while (curr != start);
+}
+
+bool path_finding2(glm::vec2 start_vec, glm::vec2 goal_vec, std::vector<glm::vec3>& poss)
+{
+    auto pos       = world_to_grid(start_vec, 32);
+    auto goal_grid = world_to_grid(goal_vec, 32);
+    PathGrid start{pos.x, pos.y};
+    PathGrid goal{goal_grid.x, goal_grid.y};
+
+    using pair = std::pair<float, PathGrid>;
+
+    auto priority_func = [](const pair& a, const pair& b) {
+        return a.first > b.first;
+    };
+    std::priority_queue<pair, std::vector<pair>, decltype(priority_func)> open(priority_func);
+
+    open.emplace(0.f, start);
+    a_star_grid[start] = start;
+    a_star_cost[start] = 0;
+
+    while (!open.empty())
+    {
+        PathGrid curr = open.top().second;
+        open.pop();
+        if (curr == goal)
+        {
+            reconstruct_path(start, goal, poss);
+            return true;
+        }
+        PathGrid next{};
+        float curr_low = ~0ull;
+        // TODO: Improve by first checking in the heuristic direction
+        auto max = world_to_grid(curr + 32);
+        auto min = world_to_grid(curr - 32);
+        for (int x = min.x; x < max.x; x++)
+        {
+            for (int y = min.y; y < max.y; y++)
+            {
+                next.x         = x;
+                next.y         = y;
+                float new_cost = a_star_cost[curr] + 1; // + cost of graph node needs to be computed
+                if (a_star_cost.find(next) == a_star_cost.end() || new_cost < a_star_cost[next])
+                {
+                    spdlog::warn("Found a node to check");
+                    a_star_cost[next] = new_cost;
+                    a_star_grid[next] = curr;
+                    float priority    = new_cost + path_heuristic(next, goal);
+
+                    open.emplace(priority, next);
+                }
+            }
+        }
+    }
+    return false;
 }
