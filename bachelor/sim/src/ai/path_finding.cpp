2fbf794581623e3464a98a8a487daee4c3ea3d03
Tomas Berger <togtja@gmail.com>
2020-02-17 10:58:20 +0100

    #55 Add simple heuristic function

--- bachelor/sim/src/ai/path_finding.cpp
+++ bachelor/sim/src/ai/path_finding.cpp
@@ -3,3 +3,10 @@ glm::ivec2 world_to_grid(glm::vec2 pos, int grid)
 {
     return {static_cast<int>(pos.x) / static_cast<int>(grid), static_cast<int>(pos.y) / static_cast<int>(grid)};
 }
+
+float path_heuristic(PathGrid pos, PathGrid pos2)
+{
+    float x = pos.x - pos2.x;
+    float y = pos.y - pos2.y;
+    return x * x + y * y;
+}
