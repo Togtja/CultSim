c445043e7768fc7d018a14b2e82331d4c8355283
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-17 14:49:25 +0100

    #46 Finish Copy constructor for action.h

--- bachelor/sim/src/entity/actions/action.h
+++ bachelor/sim/src/entity/actions/action.h
@@ -55,6 +55,10 @@ struct Action
 
         name             = other.name;
         time_to_complete = other.time_to_complete;
+        time_spent       = other.time_spent;
+        abort            = other.abort;
+        success          = other.success;
+        failure          = other.failure;
 
         requirements.clear();
         for (const auto& i : other.requirements)
