124e40e085179ad32c46665866dc4f14a80f2fe3
Tomas Berger <togtja@gmail.com>
2020-02-19 14:16:23 +0100

    #48 Add a basic key event (un-deleted)

--- bachelor/sim/src/input/input_handler_test.cpp
+++ bachelor/sim/src/input/input_handler_test.cpp
@@ -7,3 +7,13 @@ TEST_CASE("attempting to get input handler")
 {
     REQUIRE(&get_input() != nullptr);
 }
+
+TEST_CASE("attempting to create a key and use it")
+{
+    auto& input  = get_input();
+    bool success = false;
+    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_A, [&success]() { success = true; });
+    input.handle_input(SDL_SCANCODE_A);
+    CHECK(success);
+}
+
