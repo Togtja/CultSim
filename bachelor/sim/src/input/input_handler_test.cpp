3dfd1e2841c660e2e340e30507288020cbc221ba
Tomas Berger <togtja@gmail.com>
2020-02-19 15:01:32 +0100

    #48 Add test to try to use a cleared key binding

--- bachelor/sim/src/input/input_handler_test.cpp
+++ bachelor/sim/src/input/input_handler_test.cpp
@@ -17,4 +17,15 @@ TEST_CASE("attempting to create a key and use it")
     CHECK(success);
     input.clear();
 }
-
+TEST_CASE("attempting to use keybinding after clear")
+{
+    auto& input = get_input();
+    int times   = 0;
+    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F12, [&times]() { times++; });
+    input.handle_input(SDL_SCANCODE_F12);
+    input.handle_input(SDL_SCANCODE_F12);
+    CHECK(times == 2);
+    input.clear();
+    input.handle_input(SDL_SCANCODE_F12);
+    CHECK(times == 2);
+}
