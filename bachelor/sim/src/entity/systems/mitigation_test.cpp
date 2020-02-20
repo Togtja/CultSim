afbbec3537c8c837aa48f88fa0308a4f268b01f5
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-20 14:05:51 +0100

    #48 Add test case for pressing needs getting updated

--- bachelor/sim/src/entity/systems/mitigation_test.cpp
+++ bachelor/sim/src/entity/systems/mitigation_test.cpp
@@ -212,3 +212,61 @@ TEST_CASE("Test case for strategies being removed if pressing needs becomes empt
             REQUIRE(strategies.staged_strategies.empty());
         });
 }
+
+TEST_CASE("Test case that strategies change based on pressing_needs")
+{
+    entt::registry test_registry;
+
+    auto agent        = test_registry.create();
+    cs::ai::Need need = {static_cast<std::string>("hunger"), 3.f, 50.f, 0.f, static_cast<cs::tags::ETag>(cs::tags::TAG_Food)};
+
+    cs::ai::Need need2 = {static_cast<std::string>("thirst"), 8.f, 100.f, 1.f, static_cast<cs::tags::ETag>(cs::tags::TAG_Drink)};
+
+    cs::action::Action action{static_cast<std::string>("eat"),
+                              cs::tags::TAG_Find,
+                              5.f,
+                              0.f,
+                              []() { spdlog::warn("We failed to finish action: eat"); },
+                              []() { spdlog::warn("We finished action: eat"); },
+                              {}};
+
+    cs::ai::Strategy strategy = {static_cast<std::string>("eat food"),
+                                 0,
+                                 {},
+                                 cs::tags::TAG_Food,
+                                 std::vector<cs::action::Action>{std::move(action)}};
+
+    cs::ai::Strategy strategy2 = {static_cast<std::string>("drink water"),
+                                  0,
+                                  {},
+                                  static_cast<cs::tags::ETag>(cs::tags::TAG_Drink),
+                                  std::vector<cs::action::Action>{std::move(action)}};
+
+    test_registry.assign<cs::component::Needs>(agent, std::vector<cs::ai::Need>({need, need2}), std::vector<cs::ai::Need>({}));
+    test_registry.assign<cs::component::Strategies>(agent,
+                                                    std::vector<cs::ai::Strategy>({strategy, strategy2}),
+                                                    std::vector<cs::ai::Strategy>{});
+    test_registry.assign<cs::component::Tags>(agent, cs::tags::TAG_Food);
+
+    auto need_system       = new cs::system::Need(test_registry);
+    auto mitigation_system = new cs::system::Mitigation(test_registry);
+
+    need_system->update(1.f);
+    mitigation_system->update(1.f);
+    auto view = test_registry.view<cs::component::Needs, cs::component::Strategies, cs::component::Tags>();
+    view.each(
+        [strategy, strategy2](cs::component::Needs& needs, cs::component::Strategies& strategies, cs::component::Tags& tags) {
+            REQUIRE(needs.pressing_needs.size() == 1);
+            REQUIRE(strategies.staged_strategies.size() == 1);
+            REQUIRE(strategies.staged_strategies.front() == strategy);
+        });
+
+    need_system->update(50.f);
+    mitigation_system->update(1.f);
+    view.each(
+        [strategy, strategy2](cs::component::Needs& needs, cs::component::Strategies& strategies, cs::component::Tags& tags) {
+            REQUIRE(needs.pressing_needs.size() == 2);
+            REQUIRE(strategies.staged_strategies.size() == 1);
+            REQUIRE(strategies.staged_strategies.front() == strategy2);
+        });
+}
