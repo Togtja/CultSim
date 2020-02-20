24df3af1fbd351b40c481fa3364c5a61ebfd8ef7
Carl Findahl <sportleroro@gmail.com>
2020-02-20 14:25:39 +0100

    #44 Add debug control to show grid in Scen Scene

--- bachelor/sim/src/scenes/scenario_scene.cpp
+++ bachelor/sim/src/scenes/scenario_scene.cpp
@@ -83,7 +83,7 @@ void ScenarioScene::on_enter()
         m_registry.assign<component::Movement>(agent,
                                                std::vector<glm::vec3>(1, glm::vec3(rng(seed) * 100, rng(seed) * 100, 0)),
                                                glm::normalize(glm::vec2(1.f, 1.f)),
-                                               50.f);
+                                               100.f);
         m_registry.assign<component::Sprite>(agent, tex, glm::vec3(1.f, 0.f, 0.f));
         m_registry.assign<component::Vision>(agent, std::vector<entt::entity>{}, 40.f, static_cast<uint8_t>(0));
         m_registry.assign<component::Needs>(agent, std::vector<ai::Need>{need}, std::vector<ai::Need>{});
@@ -140,16 +140,22 @@ bool ScenarioScene::update(float dt)
 
 bool ScenarioScene::draw()
 {
-    constexpr int grid_size = 25;
-    for (int i = -grid_size; i <= grid_size; i++)
-    {
-        gfx::get_renderer().debug().draw_line(glm::vec3(-32 * grid_size, i * 32, 0),
-                                              glm::vec3(32 * grid_size, i * 32, 0),
-                                              glm::vec3(0.4f));
+    static bool show_grid = true;
+    ImGui::Checkbox("Show Grid", &show_grid);
 
-        gfx::get_renderer().debug().draw_line(glm::vec3(i * 32, -32 * grid_size, 0),
-                                              glm::vec3(i * 32, 32 * grid_size, 0),
-                                              glm::vec3(0.4f));
+    if (show_grid)
+    {
+        constexpr int grid_size = 25;
+        for (int i = -grid_size; i <= grid_size; i++)
+        {
+            gfx::get_renderer().debug().draw_line(glm::vec3(-32 * grid_size, i * 32, 0),
+                                                  glm::vec3(32 * grid_size, i * 32, 0),
+                                                  glm::vec3(0.3f));
+
+            gfx::get_renderer().debug().draw_line(glm::vec3(i * 32, -32 * grid_size, 0),
+                                                  glm::vec3(i * 32, 32 * grid_size, 0),
+                                                  glm::vec3(0.3f));
+        }
     }
     return false;
 }
