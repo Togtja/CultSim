773ca070c73b02c5af1b6f71ac092b3b0dcad802
Carl Findahl <sportleroro@gmail.com>
2020-02-21 08:34:46 +0100

    #58 Improve timer layout

--- bachelor/sim/src/debug/auto_timer.cpp
+++ bachelor/sim/src/debug/auto_timer.cpp
@@ -6,12 +6,17 @@ namespace cs
 {
 void AutoTimer::show_debug_ui()
 {
-    if (ImGui::TreeNode("Debug Timers"))
+    ImGui::BeginTable("Debug Timers", 2, ImGuiTableFlags_BordersOuter | ImGuiTableFlags_Sortable);
+    ImGui::TableSetupColumn("Timer");
+    ImGui::TableSetupColumn("Time (ms)", ImGuiTableColumnFlags_DefaultSort | ImGuiTableColumnFlags_PreferSortDescending);
+    ImGui::TableAutoHeaders();
+    for (auto&& [k, v] : s_results)
     {
-        for (auto&& [k, v] : s_results)
-        {
-            ImGui::Text("%s: %.2lfms", k.data(), v);
-        }
+        ImGui::TableNextCell();
+        ImGui::Text("%s", k.data());
+        ImGui::TableNextCell();
+        ImGui::TextColored({0.3f, 1.f, 0.66f, 1.f}, "%.2lf", v);
     }
+    ImGui::EndTable();
 }
 } // namespace cs
