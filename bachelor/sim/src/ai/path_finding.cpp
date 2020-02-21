999b915de11ddeea8fcef78c3ebc82a287eae8d9
Tomas Berger <togtja@gmail.com>
2020-02-21 10:00:49 +0100

    #44 Rename some variables to make more sense

--- bachelor/sim/src/ai/path_finding.cpp
+++ bachelor/sim/src/ai/path_finding.cpp
@@ -16,13 +16,13 @@ int path_heuristic(glm::ivec2 start, glm::ivec2 goal)
 }
 
 void reconstruct_path(const glm::ivec2& start,
-                      const glm::ivec2& goal,
-                      const glm::vec2& grid_goal,
+                      const glm::ivec2& grid_goal,
+                      const glm::vec2& goal,
                       std::vector<glm::vec3>& pos,
                       const robin_hood::unordered_flat_map<glm::ivec2, glm::ivec2>& a_star_grid)
 {
-    glm::ivec2 curr = goal;
-    pos.emplace_back(glm::vec3(grid_goal, 0.f));
+    pos.emplace_back(glm::vec3(goal, 0.f));
+    glm::ivec2 curr = grid_goal;
     do
     {
         pos.emplace_back(glm::vec3(curr * 32, 0));
--- bachelor/sim/src/ai/path_finding.h
+++ bachelor/sim/src/ai/path_finding.h
@@ -16,8 +16,8 @@ glm::ivec2 world_to_grid(const glm::vec2& pos, int grid = SIM_GRID_SIZE);
 
 int path_heuristic(glm::ivec2 start, glm::ivec2 goal);
 void reconstruct_path(const glm::ivec2& start,
-                      const glm::ivec2& goal,
-                      const glm::vec2& grid_goal,
+                      const glm::ivec2& grid_goal,
+                      const glm::vec2& goal,
                       std::vector<glm::vec3>& pos,
                       const robin_hood::unordered_flat_map<glm::ivec2, glm::ivec2>& a_star_grid);
 
