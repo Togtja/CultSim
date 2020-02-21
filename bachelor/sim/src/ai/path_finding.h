c175b68bc00de5d4f098f99a15675a1bd072761d
Tomas Berger <togtja@gmail.com>
2020-02-21 09:35:57 +0100

    #49 Change to use const and ref where applicable in path_finding

--- bachelor/sim/src/ai/path_finding.h
+++ bachelor/sim/src/ai/path_finding.h
@@ -12,14 +12,14 @@
 
 namespace cs::ai
 {
-glm::ivec2 world_to_grid(glm::vec2 pos, int grid = SIM_GRID_SIZE);
+glm::ivec2 world_to_grid(const glm::vec2& pos, int grid = SIM_GRID_SIZE);
 
 int path_heuristic(glm::ivec2 start, glm::ivec2 goal);
 void reconstruct_path(const glm::ivec2& start,
                       const glm::ivec2& goal,
-                      const glm::vec2& actual_goal,
+                      const glm::vec2& grid_goal,
                       std::vector<glm::vec3>& pos,
                       const robin_hood::unordered_flat_map<glm::ivec2, glm::ivec2>& a_star_grid);
 
-bool path_finding(glm::vec2 start_vec, glm::vec2 goal_vec, std::vector<glm::vec3>& poss);
+bool path_finding(const glm::vec2& start_vec, const glm::vec2& goal_vec, std::vector<glm::vec3>& poss);
 } // namespace cs::ai
