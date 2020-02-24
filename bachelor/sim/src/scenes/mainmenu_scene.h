c7efacaeaea52a26ae519d4a90d7ed6b836776fd
Carl Findahl <sportleroro@gmail.com>
2020-02-24 10:22:46 +0100

    #61 Add launch scene and quit button to main menu

--- bachelor/sim/src/scenes/mainmenu_scene.h
+++ bachelor/sim/src/scenes/mainmenu_scene.h
@@ -2,11 +2,21 @@
 
 #include "scene.h"
 
+#include <glm/vec2.hpp>
+
 namespace cs
 {
+struct Preference;
+
 class MainMenuScene : public IScene
 {
 private:
+    /** Keep track of menu size relative to screen */
+    glm::ivec2 m_main_menu_size{};
+
+    /** Keep track of window size */
+    glm::ivec2 m_window_size{};
+
 public:
     void on_enter() override;
 
@@ -15,5 +25,8 @@ public:
     bool update(float dt) override;
 
     bool draw() override;
+
+private:
+    void handle_size_changed(const Preference& before, const Preference& after);
 };
 } // namespace cs
