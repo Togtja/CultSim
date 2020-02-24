359937019a0eec86c44744ecfc526c2b03cc5405
Tomas Berger <togtja@gmail.com>
2020-02-24 14:37:41 +0100

    #63 Update the tests for new ContextHandler system

--- bachelor/sim/src/input/input_handler_test.cpp
+++ bachelor/sim/src/input/input_handler_test.cpp
@@ -12,17 +12,16 @@ TEST_CASE("attempting to create a key and use it")
 {
     auto& input  = get_input();
     bool success = false;
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_A, [&success]() { success = true; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_A, Action::MoveLeft, [&success]() { success = true; });
     input.handle_input(SDL_SCANCODE_A);
     CHECK(success);
     input.clear();
 }
-
 TEST_CASE("attempting to use keybinding after clear")
 {
     auto& input = get_input();
     int times   = 0;
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F12, [&times]() { times++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F12, Action::ZoomOut, [&times]() { times++; });
     input.handle_input(SDL_SCANCODE_F12);
     input.handle_input(SDL_SCANCODE_F12);
     CHECK(times == 2);
@@ -37,8 +36,8 @@ TEST_CASE("attempting to use keybinding after unbind")
     int times1 = 0, times0 = 0;
     // Binding 2 key to make sure that no other key also get unbinding
     // AKA somebody just "cleared" all
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F11, [&times1]() { times1++; });
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F10, [&times0]() { times0++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F11, Action::MoveLeft, [&times1]() { times1++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F10, Action::ZoomIn, [&times0]() { times0++; });
     input.handle_input(SDL_SCANCODE_F11);
     input.handle_input(SDL_SCANCODE_F10);
     CHECK(times1 == 1);
@@ -51,38 +50,34 @@ TEST_CASE("attempting to use keybinding after unbind")
     input.clear();
 }
 
-TEST_CASE("attempting to bind same key to different function without overwrite")
+TEST_CASE("attempting to bind same key to different function")
 {
     auto& input = get_input();
     int times1 = 0, times0 = 0;
 
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, [&times1]() { times1++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, Action::MoveLeft, [&times1]() { times1++; });
     input.handle_input(SDL_SCANCODE_F19);
     CHECK(times1 == 1);
 
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, [&times0]() { times0++; });
-    input.handle_input(SDL_SCANCODE_F19);
-    CHECK(times1 == 2);
-    CHECK(times0 == 0);
-    input.clear();
-}
-TEST_CASE("attempting to bind same key to different function with overwrite")
-{
-    auto& input = get_input();
-    int times1 = 0, times0 = 0;
-
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, [&times1]() { times1++; });
-    input.handle_input(SDL_SCANCODE_F19);
-    CHECK(times1 == 1);
-
-    input.bind_key(
-        KeyContext::DefaultContext,
-        SDL_SCANCODE_F19,
-        [&times0]() { times0++; },
-        true);
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, Action::MoveLeft, [&times0]() { times0++; });
     input.handle_input(SDL_SCANCODE_F19);
     CHECK(times1 == 1);
     CHECK(times0 == 1);
+    input.clear();
+}
+
+TEST_CASE("attempting to bind diffrent key to same action")
+{
+    auto& input = get_input();
+    int times   = 0;
+
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, Action::MoveLeft, [&times]() { times++; });
+    input.handle_input(SDL_SCANCODE_F19);
+    CHECK(times == 1);
+
+    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F18, Action::MoveLeft);
+    input.handle_input(SDL_SCANCODE_F18);
+    CHECK(times == 2);
     input.clear();
 }
 
@@ -90,7 +85,7 @@ TEST_CASE("attempting to unbind something that is already unbound")
 {
     auto& input = get_input();
     int times   = 0;
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, [&times]() { times++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F19, Action::MoveLeft, [&times]() { times++; });
     input.handle_input(SDL_SCANCODE_F19);
     CHECK(times == 1);
 
@@ -109,8 +104,8 @@ TEST_CASE("attempting to add default context on top of something else")
     auto& input = get_input();
     int times1 = 0, times0 = 0;
 
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F13, [&times1]() { times1++; });
-    input.bind_key(KeyContext::Agent, SDL_SCANCODE_F13, [&times0]() { times0++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F13, Action::MoveLeft, [&times1]() { times1++; });
+    input.fast_bind_key(KeyContext::Agent, SDL_SCANCODE_F13, Action::MoveLeft, [&times0]() { times0++; });
     input.handle_input(SDL_SCANCODE_F13);
     CHECK(times1 == 1);
     CHECK(times0 == 0);
@@ -131,8 +126,8 @@ TEST_CASE("attempting to add non default context on top of another one")
     auto& input = get_input();
     int times1 = 0, times0 = 0;
 
-    input.bind_key(KeyContext::Agent, SDL_SCANCODE_F13, [&times1]() { times1++; });
-    input.bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F13, [&times0]() { times0++; });
+    input.fast_bind_key(KeyContext::Agent, SDL_SCANCODE_F13, Action::MoveLeft, [&times1]() { times1++; });
+    input.fast_bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F13, Action::MoveLeft, [&times0]() { times0++; });
     // Nothing should happend as the context are not on the stack
     input.handle_input(SDL_SCANCODE_F13);
     CHECK(times1 == 0);
@@ -159,7 +154,7 @@ TEST_CASE("attempting to remove default context from stack")
     auto& input = get_input();
     int times   = 0;
 
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F24, [&times]() { times++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F24, Action::MoveLeft, [&times]() { times++; });
     input.handle_input(SDL_SCANCODE_F24);
     CHECK(times == 1);
 
@@ -174,7 +169,7 @@ TEST_CASE("attempting to pop default context from stack")
     auto& input = get_input();
     int times   = 0;
 
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F24, [&times]() { times++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F24, Action::MoveLeft, [&times]() { times++; });
     input.handle_input(SDL_SCANCODE_F24);
     CHECK(times == 1);
 
@@ -189,8 +184,8 @@ TEST_CASE("attempting to remove non default context from stack by context")
     auto& input = get_input();
     int times1 = 0, times0 = 0;
 
-    input.bind_key(KeyContext::Agent, SDL_SCANCODE_F11, [&times1]() { times1++; });
-    input.bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F13, [&times0]() { times0++; });
+    input.fast_bind_key(KeyContext::Agent, SDL_SCANCODE_F11, Action::MoveLeft, [&times1]() { times1++; });
+    input.fast_bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F13, Action::MoveLeft, [&times0]() { times0++; });
     // Nothing should happend as the context are not on the stack
     input.handle_input(SDL_SCANCODE_F11);
     input.handle_input(SDL_SCANCODE_F13);
@@ -220,8 +215,8 @@ TEST_CASE("attempting to remove top non default context from stack")
 {
     auto& input = get_input();
     int times1 = 0, times0 = 0;
-    input.bind_key(KeyContext::Agent, SDL_SCANCODE_F11, [&times1]() { times1++; });
-    input.bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F13, [&times0]() { times0++; });
+    input.fast_bind_key(KeyContext::Agent, SDL_SCANCODE_F11, Action::MoveLeft, [&times1]() { times1++; });
+    input.fast_bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F13, Action::MoveLeft, [&times0]() { times0++; });
     // Nothing should happend as the context are not on the stack
     input.handle_input(SDL_SCANCODE_F11);
     input.handle_input(SDL_SCANCODE_F13);
@@ -252,7 +247,7 @@ TEST_CASE("attempting to clear context from all bindings")
     auto& input = get_input();
     int times   = 0;
 
-    input.bind_key(KeyContext::Agent, SDL_SCANCODE_F11, [&times]() { times++; });
+    input.fast_bind_key(KeyContext::Agent, SDL_SCANCODE_F11, Action::MoveLeft, [&times]() { times++; });
     // Nothing should happend as the context are not on the stack
     input.handle_input(SDL_SCANCODE_F11);
     CHECK(times == 0);
@@ -265,7 +260,7 @@ TEST_CASE("attempting to clear context from all bindings")
     input.handle_input(SDL_SCANCODE_F11);
     CHECK(times == 1);
     // Note how we don't need to add it to the context again
-    input.bind_key(KeyContext::Agent, SDL_SCANCODE_F11, [&times]() { times++; });
+    input.fast_bind_key(KeyContext::Agent, SDL_SCANCODE_F11, Action::MoveLeft, [&times]() { times++; });
     input.handle_input(SDL_SCANCODE_F11);
     CHECK(times == 2);
     input.clear();
@@ -276,9 +271,9 @@ TEST_CASE("attempting to go back to default context")
     auto& input = get_input();
     int times0 = 0, times1 = 0, times2 = 0;
 
-    input.bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F11, [&times0]() { times0++; });
-    input.bind_key(KeyContext::Agent, SDL_SCANCODE_F11, [&times1]() { times1++; });
-    input.bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F11, [&times2]() { times2++; });
+    input.fast_bind_key(KeyContext::DefaultContext, SDL_SCANCODE_F11, Action::MoveLeft, [&times0]() { times0++; });
+    input.fast_bind_key(KeyContext::Agent, SDL_SCANCODE_F11, Action::MoveLeft, [&times1]() { times1++; });
+    input.fast_bind_key(KeyContext::AgentOnHover, SDL_SCANCODE_F11, Action::MoveLeft, [&times2]() { times2++; });
     // Only deafult is on the context stack
     input.handle_input(SDL_SCANCODE_F11);
     CHECK(times0 == 1);
