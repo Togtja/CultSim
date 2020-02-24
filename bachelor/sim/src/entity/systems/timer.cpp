b7a53a6593bccd50c380d1557527c8a7693ccfb7
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-24 13:51:59 +0100

    #47 Finish up first code for Timer functionality

--- bachelor/sim/src/entity/systems/timer.cpp
+++ bachelor/sim/src/entity/systems/timer.cpp
@@ -10,7 +10,21 @@ void Timer::update(float dt)
 
     auto group = m_registry.group<component::Timer>();
     group.each([this, dt](entt::entity e, component::Timer& timer) {
+        timer.time_spent += dt;
+        if (timer.time_spent > timer.time_to_complete)
+        {
+            timer.number_of_loops--;
+            timer.on_complete();
+            if (timer.number_of_loops > 0)
+            {
+                timer.number_of_loops--;
+            }
+        }
 
+        if (timer.number_of_loops == 0) 
+        {
+            m_registry.remove<component::Timer>(e);
+        }
     });
 }
 } // namespace cs::system
