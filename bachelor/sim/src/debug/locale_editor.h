a75e4a6ef121af89a76849df41ddef7cf37ac951
Carl Findahl <sportleroro@gmail.com>
2020-02-24 14:49:05 +0100

    #68 Finalize initial version of locale editor (no save yet)

--- bachelor/sim/src/debug/locale_editor.h
+++ bachelor/sim/src/debug/locale_editor.h
@@ -12,12 +12,11 @@ class LocaleEditor
 private:
     inline static constexpr int MAX_STR_LEN = 128;
 
-    sol::state_view m_lua;
     std::vector<std::string> m_locales{};
-    robin_hood::unordered_flat_map<std::string, std::vector<std::string>> m_translations{};
+    robin_hood::unordered_map<std::string, std::vector<std::string>> m_translations{};
 
 public:
-    explicit LocaleEditor(sol::state_view lua);
+    void init(sol::state_view lua);
 
     void show();
 };
