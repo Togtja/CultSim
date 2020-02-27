8d944d4c097d0253ece2cee8e71f667d94158bb8
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-02-27 09:28:09 +0100

    #44 Add documentation to various systems

--- bachelor/sim/src/entity/systems/action.h
+++ bachelor/sim/src/entity/systems/action.h
@@ -7,6 +7,16 @@
 
 namespace cs::system
 {
+/**
+* Action system goes through each entity and runs the actions in the first strategy of the staged strategy list
+*
+* The Action system goes through each entity, and finds the first strategy in the list of staged strategies.
+* It then goes to the first action, and if it has a requirement, it adds a corresponding component to the entity
+* The action system then ignores entities with requirement components
+* Once the requirement has been taken care off, the action system begins ticking down the time left on the action
+* When the action is done, the system then runs a check to see if the action succeeds, and then removes it from the list of actions
+* Once the strategy is out of actions, if the need has not yet been fullfilled, the next action is chosen
+*/
 class Action : public ISystem
 {
 private:
