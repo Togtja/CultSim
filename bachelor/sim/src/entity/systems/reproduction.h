bf3d26f304d6c98d8c60e27afff6d9c4678768bc
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-02 10:10:12 +0100

    #44 Add more documentation to the new decision making systems

--- bachelor/sim/src/entity/systems/reproduction.h
+++ bachelor/sim/src/entity/systems/reproduction.h
@@ -4,6 +4,16 @@
 
 namespace cs::system
 {
+/**
+* System that enforces a limit to the amount of children an entity may have
+*
+* The Reproduction system handles the background cleanup for reproduction started in the reproduction action
+* The System counts the number of children an entity has had, and once that is reached, removes the need and component associated with reproduction
+*
+* Will likely handle pregnancy and similar things associated with reproduction
+*
+*@Note Despite it's name, the reproduction system is not required for reproduction to function at the moment, only for entities to eventually stop reproducing. Reproduction is handled by the corresponding action
+*/
 class Reproduction : public ISystem
 {
 public:
