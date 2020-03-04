dc0cff693da678eab306e6ff96e049a273b1c920
Tomas Berger <tomashb@ntnu.no>
2020-03-04 15:26:06 +0100

    #69 Add Keybinding lua file

--- bachelor/sim/res/script/keybinding.lua
+++ bachelor/sim/res/script/keybinding.lua
@@ -1,5 +1,18 @@
 -- Lua configuration file for the application
 
-action_bindings = {
-    "x" = EAction.ZoomIn,
+action_key_bindings = {
+    [EKeyContext.ScenarioScene] = {
+        ["q"] = EAction.ZoomIn,
+        ["e"] = EAction.ZoomOut,
+        ["w"] = EAction.MoveUp,
+        ["a"] = EAction.MoveLeft,
+        ["s"] = EAction.MoveDown,
+        ["d"] = EAction.MoveRight
+    }
+}
+action_mouse_bindings = {
+    [EKeyContext.ScenarioScene] = {
+        [EMouse.WheelUp] = EAction.ZoomIn,
+        [EMouse.WheelDown] = EAction.ZoomOut
+    }
 }
