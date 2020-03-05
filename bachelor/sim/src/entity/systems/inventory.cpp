78ae587c574bd2adae699521836344c3f4ba579c
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-05 10:39:40 +0100

    #73 Finish up preliminary Inventory system

--- bachelor/sim/src/entity/systems/inventory.cpp
+++ bachelor/sim/src/entity/systems/inventory.cpp
@@ -1,8 +1,28 @@
 #include "inventory.h"
+#include "debug/auto_timer.h"
+#include "entity/components/components.h"
+
+#include "spdlog/spdlog.h"
 
 namespace cs::system
 {
 void Inventory::update(float dt)
 {
+    CS_AUTOTIMER(Inventory System);
+
+    auto& registry = *m_context.registry;
+    auto group     = registry.group<component::Tags, component::Inventory>();
+
+    group.each([this, dt, &registry](entt::entity e, component::Inventory& inventory) {
+        if (inventory.size != inventory.contents.size())
+        {
+            inventory.size = inventory.contents.size();
+            for (entt::entity component : inventory.contents)
+            {
+                auto component_tags = registry.get<component::Tags>(component);
+                inventory.tags      = ETag(inventory.tags | component_tags.tags);
+            }
+        }
+    });
 }
 } // namespace cs::system
