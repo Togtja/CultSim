9b778c0dfa5234a412fba178190d3860fddcc4d6
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-06 19:54:10 +0100

    #75 Add EntityMemory

---
+++ bachelor/sim/src/entity/memories/entity_memory.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "memory.h"
+
+#include <glm/vec3.hpp>
+#include <entt/entity/registry.hpp>
+
+namespace cs
+{
+class EntityMemory : public IMemory
+{
+    EntityMemory(ETag tags, float time_of_creation, glm::vec3 location, entt::entity target) :
+        IMemory(tags, time_of_creation),
+        m_location(location),
+        m_target(target){};
+
+    glm::vec3 m_location{};
+    entt::entity m_target = entt::null;
+
+};
+} // namespace cs
