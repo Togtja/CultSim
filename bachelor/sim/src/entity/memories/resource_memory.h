2baae82b529b9abad7b885f53bd5a60cb603a3ed
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-08 10:54:29 +0100

    #75 Add sorting function to Resource Memory

--- bachelor/sim/src/entity/memories/resource_memory.h
+++ bachelor/sim/src/entity/memories/resource_memory.h
@@ -17,5 +17,26 @@ public:
     int m_number_of_matching_entities{};
 
     void update(float dt) override{};
+
+    static ResourceMemory sort(ResourceMemory const& lhs, ResourceMemory const& rhs, entt::entity e, entt::registry& r)
+    {
+        float cost_lhs{};
+        float cost_rhs{};
+
+        if (r.valid(e))
+        {
+            auto pos = r.get<component::Position>(e);
+            cost_lhs += glm::distance(pos.position, lhs.m_location);
+            cost_rhs += glm::distance(pos.position, rhs.m_location);
+
+            cost_lhs -= 10 * lhs.m_number_of_matching_entities;
+            cost_rhs -= 10 * rhs.m_number_of_matching_entities;
+            if (cost_lhs > cost_rhs) 
+            {
+                return rhs;
+            }
+        }
+            return lhs;
+    }
 };
 } // namespace cs
