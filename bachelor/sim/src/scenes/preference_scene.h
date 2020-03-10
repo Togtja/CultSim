4a8bed098d499c225a72a0c12c2fa9aa708a31f4
Tomas Berger <togtja@gmail.com>
2020-03-10 09:12:12 +0100

    #82 Add headers for the prefrences

--- bachelor/sim/src/scenes/preference_scene.h
+++ bachelor/sim/src/scenes/preference_scene.h
@@ -11,7 +11,23 @@ namespace cs
 class PreferenceScene : public IScene
 {
 private:
+    robin_hood::unordered_map<input::EKeyContext, input::detail::ActionHandler> m_key_map;
+    robin_hood::unordered_map<input::EKeyContext, std::vector<std::string>> m_key_buff;
+    int free_codes = 287;
+
 public:
+    PreferenceScene()
+    {
+        m_key_map = input::get_input().get_input_map();
+        for (auto&& [context, action_h] : m_key_map)
+        {
+            for (auto&& [key, _] : action_h.get_key_binding())
+            {
+                m_key_buff[context].push_back({SDL_GetScancodeName(key)});
+            }
+        }
+    }
+
     void on_enter() override;
 
     void on_exit() override;
@@ -19,3 +35,7 @@ public:
     bool update(float dt) override;
 
     bool draw() override;
+
+    void key_binding();
+};
+} // namespace cs
