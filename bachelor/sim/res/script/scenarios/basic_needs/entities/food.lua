0fcbddbdeffc7b53d07d5cd590b80e2bf2495ebb
Carl Findahl <sportleroro@gmail.com>
2020-03-13 16:07:43 +0100

    #69 Add Food Source in Lua

---
+++ bachelor/sim/res/script/scenarios/basic_needs/entities/food.lua
@@ -0,0 +1,12 @@
+entity = {
+    PositionComponent = {
+        position = Vec2:new(0.0, 0.0)
+    },
+    SpriteComponent = {
+        color = Vec3:new(0.20, 1.00, 0.10),
+        texture = "sprites/food_c.png"
+    },
+    TagComponent = {
+        tags = ETag.Food
+    }
+}
