afe7966f3efe295e0475f6ccf4ac1403f35e1d17
Carl Findahl <sportleroro@gmail.com>
2020-03-13 16:13:00 +0100

    #69 Add water and water source in Lua

---
+++ bachelor/sim/res/script/scenarios/basic_needs/entities/water.lua
@@ -0,0 +1,12 @@
+entity = {
+    PositionComponent = {
+        position = Vec2:new(0.0, 0.0)
+    },
+    SpriteComponent = {
+        color = Vec3:new(0.10, 0.70, 1.00),
+        texture = "sprites/liquid_c.png"
+    },
+    TagComponent = {
+        tags = ETag.Drink
+    }
+}
---
+++ bachelor/sim/res/script/scenarios/basic_needs/entities/water_source.lua
@@ -0,0 +1,22 @@
+entity = {
+    PositionComponent = {
+        position = Vec2:new(random:uniform(-scenario.bounds.x, scenario.bounds.x),
+                            random:uniform(-scenario.bounds.y, scenario.bounds.y))
+    },
+    SpriteComponent = {
+        color = Vec3:new(0.10, 0.20, 0.60),
+        texture = "sprites/circle.png"
+    },
+    TagComponent = {
+        tags = ETag.Avoidable
+    },
+    TimerComponent = {
+        time_to_complete = random:uniform(25.0, 120.0),
+        number_of_loops = -1,
+        on_complete = function(e)
+            local my_pos = cultsim.get_component(e, component.position)
+            cultsim.spawn_at("water", Vec2:new(random:uniform(my_pos.position.x - 10.0, my_pos.position.x + 10.0),
+                                              random:uniform(my_pos.position.y - 10.0, my_pos.position.y + 10.0)))
+        end
+    }
+}
