f843a9eb263eafd98478c05e0f5a1852a80aa590
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-17 13:34:42 +0100

    #83 Moved chance of action success into action struct

---
+++ bachelor/sim/src/entity/actions/action.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "entity/components/tags.h"
+
+#include <functional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <entt/entt.hpp>
+#include <sol/sol.hpp>
+
+namespace cs::action
+{
+struct Action
+{
+    std::string name{};
+
+    ETag requirements{};
+
+    float time_to_complete{};
+    float time_spent = 0.f;
+
+    float success_chance = 1.f;
+
+    entt::entity target{};
+
+    sol::function success{};
+    sol::function failure{};
+    std::function<void(void)> abort{};
+};
+
+inline bool operator==(Action const& lhs, Action const& rhs)
+{
+    return lhs.name == rhs.name;
+}
+} // namespace cs::action
