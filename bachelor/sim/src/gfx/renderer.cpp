41be17f59f733d3f80f7518a94fb14432194c1e3
Carl Findahl <sportleroro@gmail.com>
2020-03-18 17:39:33 +0100

    #86 Create set camera position in renderer

--- bachelor/sim/src/gfx/renderer.cpp
+++ bachelor/sim/src/gfx/renderer.cpp
@@ -32,9 +32,9 @@ const SpriteRenderer& Renderer::sprite() const
     return m_sprite_renderer;
 }
 
-void Renderer::set_camera_position(glm::vec3 pos)
+void Renderer::set_camera_position(const glm::vec3& pos)
 {
-    /** Todo */
+    m_camera.set_position(pos);
 }
 
 void Renderer::move_camera(glm::vec3 delta)
