32ab136f747238d91ef14499fa726431a4b43c4f
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-19 14:39:59 +0100

    #83 Add check to see if entity has age component and subsequent updated health regeneration

--- bachelor/sim/src/entity/systems/health.cpp
+++ bachelor/sim/src/entity/systems/health.cpp
@@ -10,8 +10,8 @@ void Health::update(float dt)
 {
     CS_AUTOTIMER(Health System);
 
-    auto view = m_context.registry->view<component::Health, component::Need, component::Tags>();
-    view.each([this, dt](entt::entity e, component::Health& health, component::Need& need, component::Tags tags) {
+    auto view = m_context.registry->view<component::Health, component::Need>();
+    view.each([this, dt](entt::entity e, component::Health& health, component::Need& need) {
         for (auto& need : need.vital_needs)
         {
             if ((need.tags & health.need_tags) && need.status <= 0.f)
@@ -24,11 +24,25 @@ void Health::update(float dt)
             }
         }
 
+        // TODO: Better entity removal upon death
         if (health.health <= 0.f)
         {
             m_context.registry->destroy(e);
         }
-        health.health = std::clamp(health.health + dt * health.tickdown_rate * 0.1f, 0.f, 100.f);
+        
+        if (auto age = m_context.registry->try_get<component::Age>(e); age)
+        {
+            age->current_age += dt;
+
+            auto age_above_max = age->current_age - age->max_age;
+            health.health =
+                std::clamp(health.health + dt * health.tickdown_rate * 0.1f, 0.f, 100.f - (age->max_age - age->current_age));
+        }
+        else
+        {
+            health.health = std::clamp(health.health + dt * health.tickdown_rate * 0.1f, 0.f, 100.f);
+        }
+
     });
 }
 } // namespace cs::system
