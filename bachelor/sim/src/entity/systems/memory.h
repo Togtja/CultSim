af5f69de2cc773fb4c4182dba5a2a00ec517aa48
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-19 14:12:34 +0100

    #83 Add new variables to memory.h for improved memory retention

--- bachelor/sim/src/entity/systems/memory.h
+++ bachelor/sim/src/entity/systems/memory.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "system.h"
-
+#include "entity/events.h"
 namespace cs::system
 {
 class Memory : public ISystem
@@ -8,9 +8,22 @@ class Memory : public ISystem
 private:
     float m_timer{};
 
-public:
-    using ISystem::ISystem;
+    float m_max_memories{};
 
+    float m_max_retention_time{};
+
+public:
+    Memory(SystemContext context) : ISystem(context)
+    {
+        context.dispatcher->sink<event::CreatedMemory>().connect<&Memory::update_memories>(this);
+    }
+
+    ~Memory()
+    {
+        m_context.dispatcher->sink<event::CreatedMemory>().disconnect<&Memory::update_memories>(this);
+    }
     void update(float dt) override;
+
+    void update_memories(const event::CreatedMemory& event);
 };
 } // namespace cs::system
