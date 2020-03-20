deef159960943df3bb86f8de4d3d3c7719063636
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-20 10:33:56 +0100

    #87 Finish up Deletion system for now

--- bachelor/sim/src/entity/systems/deletion.cpp
+++ bachelor/sim/src/entity/systems/deletion.cpp
@@ -1,9 +1,31 @@
 #include "deletion.h"
+#include "debug/auto_timer.h"
 #include "entity/components/components.h"
 
 namespace cs::system
 {
 void Deletion::update(float dt)
 {
+    CS_AUTOTIMER(Deletion System);
+
+    auto& registry = *m_context.registry;
+
+    auto view = registry.view<component::Delete>();
+    view.each([this, dt](entt::entity e, component::Delete& deletion) {
+        deletion.time_since_marked += dt;
+
+        if (deletion.time_since_marked >= deletion.time_before_timeout)
+        {
+            m_context.registry->remove<component::Delete>(e);
+        }
+    });
+}
+void Deletion::check_and_delete(const event::DeleteEntity& event)
+{
+    if (m_context.registry->has<component::Delete>(event.entity)) 
+    {
+        m_context.dispatcher->enqueue<event::RemovedEntity>(event.entity);
+        m_context.registry->destroy(event.entity);
+    }
 }
 } // namespace cs::system
