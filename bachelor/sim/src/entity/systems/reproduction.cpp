3d04ad4f8c25bd2b71628f7de88067f8e0b81384
Carl Findahl <sportleroro@gmail.com>
2020-03-20 12:04:37 +0100

    #85 Capitalize timer name in repro system

--- bachelor/sim/src/entity/systems/reproduction.cpp
+++ bachelor/sim/src/entity/systems/reproduction.cpp
@@ -6,11 +6,10 @@ namespace cs::system
 {
 void Reproduction::update(float dt)
 {
-    CS_AUTOTIMER(reproduction System);
+    CS_AUTOTIMER(Reproduction System);
 
     auto view = m_context.registry->view<component::Reproduction, component::Need>();
     view.each([dt, this](const entt::entity e, component::Reproduction& repr, component::Need& needs) {
-        
         if (auto age = m_context.registry->try_get<component::Age>(e); age)
         {
             std::clamp(repr.fertility = 100.f - (age->current_age - age->max_age), 0.f, 100.f);
@@ -19,7 +18,6 @@ void Reproduction::update(float dt)
         {
             repr.fertility = 100.f - (repr.number_of_children * 10.f);
         }
-
     });
 }
 } // namespace cs::system
