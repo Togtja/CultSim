959da00ee317dcee1fc9beae880518c1e8d19d40
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-20 11:12:00 +0100

    #83 Improve reproduction system and add pregnancy component

--- bachelor/sim/src/entity/systems/reproduction.h
+++ bachelor/sim/src/entity/systems/reproduction.h
@@ -1,24 +1,37 @@
 #pragma once
 
 #include "system.h"
+#include "entity/events.h"
 
 namespace cs::system
 {
 /**
-* System that enforces a limit to the amount of children an entity may have
-*
-* The Reproduction system handles the background cleanup for reproduction started in the reproduction action
-* The System counts the number of children an entity has had, and once that is reached, removes the need and component associated with reproduction
-*
-* Will likely handle pregnancy and similar things associated with reproduction
-*
-*@Note Despite it's name, the reproduction system is not required for reproduction to function at the moment, only for entities to eventually stop reproducing. Reproduction is handled by the corresponding action
-*/
+ * System that enforces a limit to the amount of children an entity may have
+ *
+ * The Reproduction system handles the background cleanup for reproduction started in the reproduction action
+ * The System counts the number of children an entity has had, and once that is reached, removes the need and component associated
+ *with reproduction
+ *
+ * Will likely handle pregnancy and similar things associated with reproduction
+ *
+ *@Note Despite it's name, the reproduction system is not required for reproduction to function at the moment, only for entities
+ *to eventually stop reproducing. Reproduction is handled by the corresponding action
+ */
 class Reproduction : public ISystem
 {
 public:
-    using ISystem::ISystem;
+    Reproduction(SystemContext context) : ISystem(context)
+    {
+        context.dispatcher->sink<event::RemovedEntity>().connect<&Reproduction::delete_father>(this);
+    }
+
+    ~Reproduction()
+    {
+        m_context.dispatcher->sink<event::RemovedEntity>().disconnect<&Reproduction::delete_father>(this);
+    }
 
     void update(float dt) override;
+
+    void delete_father(const event::RemovedEntity& event);
 };
 } // namespace cs::system
