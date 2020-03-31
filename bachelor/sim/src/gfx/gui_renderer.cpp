2897681f8fa2b23226a41f649858cac6e830a26c
Carl Findahl <sportleroro@gmail.com>
2020-03-31 12:16:07 +0200

    #106 Add interface for Gui renderer

--- bachelor/sim/src/gfx/gui_renderer.cpp
+++ bachelor/sim/src/gfx/gui_renderer.cpp
@@ -14,6 +14,11 @@ GuiRenderer::GuiRenderer()
     ul::Platform::instance().set_config(config);
     ul::Platform::instance().set_gpu_driver(this);
     ul::Platform::instance().set_file_system(&m_filesystem);
+
+    m_renderer = ul::Renderer::Create();
+
+    m_hello_view = m_renderer->CreateView(500, 250, false);
+    m_hello_view->LoadHTML("<h1>Hello Cultsim!</h1>");
 }
 
 } // namespace cs::gfx
--- bachelor/sim/src/gfx/gui_renderer.h
+++ bachelor/sim/src/gfx/gui_renderer.h
@@ -3,6 +3,9 @@
 #include "filesystem/gui_filesystem.h"
 
 #include <Ultralight/platform/GPUDriver.h>
+#include <Ultralight/RefPtr.h>
+#include <Ultralight/Renderer.h>
+#include <Ultralight/Bitmap.h>
 
 namespace cs::gfx
 {
@@ -11,7 +14,60 @@ class GuiRenderer : public ultralight::GPUDriver
 private:
     fs::GuiFilesystem m_filesystem{};
 
+    ultralight::RefPtr<ultralight::Renderer> m_renderer{};
+
+    ultralight::RefPtr<ultralight::View> m_hello_view{};
+
 public:
     GuiRenderer();
+
+    // GPUDriver interface
+public:
+    void BeginSynchronize() override{};
+
+    void EndSynchronize() override{};
+
+    uint32_t NextTextureId() override;
+
+    void CreateTexture(uint32_t texture_id, ultralight::Ref<ultralight::Bitmap> bitmap) override;
+
+    void UpdateTexture(uint32_t texture_id, ultralight::Ref<ultralight::Bitmap> bitmap) override;
+
+    void BindTexture(uint8_t texture_unit, uint32_t texture_id) override;
+
+    void DestroyTexture(uint32_t texture_id) override;
+
+    uint32_t NextRenderBufferId() override;
+
+    void CreateRenderBuffer(uint32_t render_buffer_id, const ultralight::RenderBuffer& buffer) override;
+
+    void BindRenderBuffer(uint32_t render_buffer_id) override;
+
+    void ClearRenderBuffer(uint32_t render_buffer_id) override;
+
+    void DestroyRenderBuffer(uint32_t render_buffer_id) override;
+
+    uint32_t NextGeometryId() override;
+
+    void CreateGeometry(uint32_t geometry_id,
+                        const ultralight::VertexBuffer& vertices,
+                        const ultralight::IndexBuffer& indices) override;
+
+    void UpdateGeometry(uint32_t geometry_id,
+                        const ultralight::VertexBuffer& vertices,
+                        const ultralight::IndexBuffer& indices) override;
+
+    void DrawGeometry(uint32_t geometry_id,
+                      uint32_t indices_count,
+                      uint32_t indices_offset,
+                      const ultralight::GPUState& state) override;
+
+    void DestroyGeometry(uint32_t geometry_id) override;
+
+    void UpdateCommandList(const ultralight::CommandList& list) override;
+
+    bool HasCommandsPending() override;
+
+    void DrawCommandList() override;
 };
 } // namespace cs::gfx
