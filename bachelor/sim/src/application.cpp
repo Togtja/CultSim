86e730f87dccbc379c283015fac6c28b2d99cbe5
Carl Findahl <sportleroro@gmail.com>
2020-03-31 09:42:54 +0200

    #44 Fix naming on I/O functions

--- bachelor/sim/src/application.cpp
+++ bachelor/sim/src/application.cpp
@@ -142,13 +142,13 @@ bool Application::init_lua()
     m_lua.set_exception_handler(&lua::exception_handler);
 
     /* Bind IO Functions (globally) */
-    m_lua.set_function("writeFile", fs::write_file);
-    m_lua.set_function("readFile", fs::read_file);
-    m_lua.set_function("makeDirectory", fs::mkdir);
-    m_lua.set_function("moveFile", fs::move_file);
-    m_lua.set_function("fileExists", fs::exists);
-    m_lua.set_function("deleteFile", fs::delete_file);
-    m_lua.set_function("copyFile", fs::copy_file);
+    m_lua.set_function("write_file", fs::write_file);
+    m_lua.set_function("read_file", fs::read_file);
+    m_lua.set_function("make_directory", fs::mkdir);
+    m_lua.set_function("move_file", fs::move_file);
+    m_lua.set_function("file_exists", fs::exists);
+    m_lua.set_function("delete_file", fs::delete_file);
+    m_lua.set_function("copy_file", fs::copy_file);
 
     /* Bind Log Functions (available in log.*) */
     auto log_table = m_lua.create_table("log");
