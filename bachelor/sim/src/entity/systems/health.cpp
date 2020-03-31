6a55dba82b2f2ad40a62d2d7fe3274de24b93687
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-03-31 13:23:55 +0200

    #103 Moved Age into it's own view for now

--- bachelor/sim/src/entity/systems/health.cpp
+++ bachelor/sim/src/entity/systems/health.cpp
@@ -35,36 +35,35 @@ void Health::update(float dt)
             return;
         }
 
-        if (auto age = m_context.registry->try_get<component::Age>(e); age)
-        {
-            m_timer += dt;
-            age->current_age += dt;
-            if (m_timer > 1.f)
-            {
-
-                m_timer           = 0.f;
-                auto death_chance = pow(sigmoid(pow(age->current_age,2) / pow(age->average_life_expectancy,2)),20);
-                auto death_roll   = m_context.rng->trigger(death_chance);
-                if (death_roll && dt != 0)
-                {
-                    spdlog::get("agent")->warn(
-                        "We killed an entity due to old age, death chance: {} , current age : {}, average age: {}",
-                        death_chance,
-                        age->current_age,
-                        age->average_life_expectancy);
-                    m_context.registry->assign<component::Delete>(e);
-                    auto tags = m_context.registry->try_get<component::Tags>(e);
-                    if (tags)
-                    {
-                        tags->tags = static_cast<ETag>(tags->tags | ETag::TAG_Delete);
-                    }
-                    return;
-                }
-            }
-        }
-        else
-        {
             health.health = std::clamp(health.health + dt * health.tickdown_rate * 0.1f, 0.f, 100.f);
+
+    });
+
+    auto age_view = m_context.registry->view<component::Age>();
+    view.each([this, dt](entt::entity e, component::Age& age) {
+        m_timer += dt;
+        age.current_age += dt;
+
+        if (m_timer > 1.f)
+        {
+            m_timer           = 0.f;
+            auto death_chance = pow(sigmoid(pow(age.current_age, 2) / pow(age.average_life_expectancy, 2)), 20);
+            auto death_roll   = m_context.rng->trigger(death_chance);
+            if (death_roll && dt != 0)
+            {
+                spdlog::get("agent")->warn(
+                    "We killed an entity due to old age, death chance: {} , current age : {}, average age: {}",
+                    death_chance,
+                    age.current_age,
+                    age.average_life_expectancy);
+                m_context.registry->assign<component::Delete>(e);
+                auto tags = m_context.registry->try_get<component::Tags>(e);
+                if (tags)
+                {
+                    tags->tags = static_cast<ETag>(tags->tags | ETag::TAG_Delete);
+                }
+                return;
+            }
         }
     });
 }
