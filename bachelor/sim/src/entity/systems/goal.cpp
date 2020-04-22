b31049f68518e0c6132bf0e66b7d28099ff0bd26
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-04-22 15:14:54 +0200

    #97 Add skeleton code for goal system

--- bachelor/sim/src/entity/systems/goal.cpp
+++ bachelor/sim/src/entity/systems/goal.cpp
@@ -1,1 +1,24 @@
 #include "goal.h"
+#include "debug/auto_timer.h"
+#include "entity/components/components.h"
+
+namespace cs::system
+{
+void Goal::update(float dt)
+{
+    CS_AUTOTIMER(Deletion System);
+
+    auto& registry = *m_context.registry;
+
+    auto view = registry.view<component::Goals>();
+    view.each([this, dt](entt::entity e) 
+        {
+
+        });
+}
+
+ISystem* Goal::clone()
+{
+   return new Goal(m_context);
+}
+} // namespace cs::system
--- bachelor/sim/src/entity/systems/goal.h
+++ bachelor/sim/src/entity/systems/goal.h
@@ -1,2 +1,15 @@
 #pragma once
 #include "system.h"
+
+namespace cs::system
+{
+class Goal : public ISystem
+{
+public:
+    using ISystem::ISystem;
+
+    void update(float dt) override;
+
+    ISystem* clone() override;
+};
+} // namespace cs::system
