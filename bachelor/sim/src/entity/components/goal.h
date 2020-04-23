25428c0296fd09af144b78993d89b12c4670e2bd
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-04-23 14:13:48 +0200

    #97 Fix errors in naming of variables

--- bachelor/sim/src/entity/components/goal.h
+++ bachelor/sim/src/entity/components/goal.h
@@ -1,22 +1,27 @@
 #pragma once
+#include "tags.h"
+
+#include <functional>
 #include <string>
 #include <variant>
+
+#include <sol/sol.hpp>
 
 namespace cs::gob
 {
 class Goal
 {
 public:
-    std::string name{};
-    ETag tags{};
+    std::string m_name{};
+    ETag m_tags{};
 
-    float weight{};
-    float age{};
+    float m_weight{};
+    float m_age{};
 
-    std::variant<sol::function, std::function<float>> weight_function = [this]() {
-        return age;
+    std::variant<sol::function, std::function<float()>> m_weight_function = [this]() {
+        return m_age;
     };
-    std::variant<sol::function, std::function<float(const float)>> change_over_time = [this](const float dt) {
+    std::variant<sol::function, std::function<float(const float)>> m_change_over_time = [this](const float dt) {
         return dt;
     };
 };
