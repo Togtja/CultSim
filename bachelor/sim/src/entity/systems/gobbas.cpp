f574011cac84f77221619e8bc7e7ec7dd885afec
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-04-24 09:52:17 +0200

    #97 Add files for Goal Oriented Behaviour Based Action System GOBBAS

---
+++ bachelor/sim/src/entity/systems/gobbas.cpp
@@ -0,0 +1,5 @@
+#include "gobbas.h"
+
+namespace cs::system
+{
+}
---
+++ bachelor/sim/src/entity/systems/gobbas.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "system.h"
+
+namespace cs::system
+{
+class GOBBAS : public ISystem
+{
+};
+} // namespace cs::system
