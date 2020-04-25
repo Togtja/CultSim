c2f64db69aefa61ab3aca00b69fa2f605cd81d54
Carl Findahl <sportleroro@gmail.com>
2020-04-25 12:57:06 +0200

    #111 Temp fix for lua system error

--- bachelor/sim/res/script/scenarios/basic_needs/02_systems.lua
+++ bachelor/sim/res/script/scenarios/basic_needs/02_systems.lua
@@ -1,6 +1,8 @@
 -- Defines custom systems
 systems = {
     be_stupid_system = cultlib.create_system("Be stupid", {
-        initialize = function() log.info("Hi, I am the stupid system!") end
+        initialize = function() log.info("Hi, I am the stupid system!") end,
+        update = function(dt) end,
+        deinitialize = function() log.info("Bye, I was the stupid system!") end
     })
 }
