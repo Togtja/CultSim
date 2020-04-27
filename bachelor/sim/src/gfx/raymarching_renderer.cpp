4c3071b675bb7f604bd9709481cefa01b59c2163
Carl Findahl <sportleroro@gmail.com>
2020-04-27 14:57:06 +0200

    #115 Add reload function to 3D renderer

--- bachelor/sim/src/gfx/raymarching_renderer.cpp
+++ bachelor/sim/src/gfx/raymarching_renderer.cpp
@@ -5,18 +5,12 @@ namespace cs::gfx
 {
 RaymarchingRenderer::RaymarchingRenderer(Camera& camera) : m_camera(camera)
 {
-    /** Init Shader */
-    auto vertex_shader = fcompile_shader("shader/raymarch.vert", GL_VERTEX_SHADER);
-    auto frag_shader   = fcompile_shader("shader/raymarch.frag", GL_FRAGMENT_SHADER);
-    m_shader           = create_program({vertex_shader, frag_shader});
-    glDeleteShader(vertex_shader);
-    glDeleteShader(frag_shader);
-    glUseProgram(m_shader);
+    init();
 }
 
 RaymarchingRenderer::~RaymarchingRenderer() noexcept
 {
-    glDeleteProgram(m_shader);
+    deinit();
 }
 
 void RaymarchingRenderer::display()
@@ -25,4 +19,23 @@ void RaymarchingRenderer::display()
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
+void RaymarchingRenderer::reload()
+{
+    deinit();
+    init();
+}
+
+void RaymarchingRenderer::init()
+{
+    auto vertex_shader = fcompile_shader("shader/raymarch.vert", GL_VERTEX_SHADER);
+    auto frag_shader   = fcompile_shader("shader/raymarch.frag", GL_FRAGMENT_SHADER);
+    m_shader           = create_program({vertex_shader, frag_shader});
+    glDeleteShader(vertex_shader);
+    glDeleteShader(frag_shader);
+}
+
+void RaymarchingRenderer::deinit()
+{
+    glDeleteProgram(m_shader);
+}
 } // namespace cs::gfx
--- bachelor/sim/src/gfx/raymarching_renderer.h
+++ bachelor/sim/src/gfx/raymarching_renderer.h
@@ -7,8 +7,10 @@ namespace cs::gfx
 class RaymarchingRenderer
 {
 private:
+    /** Reference to the camera */
     Camera& m_camera;
 
+    /** Active shader */
     uint32_t m_shader;
 
 public:
@@ -16,6 +18,19 @@ public:
 
     ~RaymarchingRenderer() noexcept;
 
+    /**
+     * Make the renderer display it's data to the screen
+     */
     void display();
+
+    /**
+     * Reload the renderer to it's initial state
+     */
+    void reload();
+
+private:
+    void init();
+
+    void deinit();
 };
 } // namespace cs::gfx
