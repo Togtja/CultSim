195ee3e278b67d430d07235f751bc8569849af14
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-04-27 14:38:06 +0200

    #116 Refactor GOB actions to no longer have a clone function and to no longer have default implementations for their action and get_goal_change functions

--- bachelor/sim/src/entity/components/action.h
+++ bachelor/sim/src/entity/components/action.h
@@ -23,19 +23,12 @@ public:
 
     uint32_t flags{};
 
-    std::variant<sol::function, std::function<float(const Goal)>> m_get_goal_change = [](const Goal goal) {
-        /**-1 for undefined */
-        return -1;
-    };
+    sol::function m_action{};
+
+    sol::function m_get_goal_change{};
 
     std::variant<sol::function, std::function<float()>> m_get_duration = [this]() {
         return m_required_time;
-    };
-
-    std::variant<sol::function, std::function<bool(entt::entity, std::string&)>> m_action = [this](entt::entity e,
-                                                                                                   std::string& error) {
-        error = fmt::format("Undefined action {} for entity {}, please define an m_action function.", m_name, e);
-        return false;
     };
 
     Action() = default;
@@ -44,20 +37,17 @@ public:
            ETag tags,
            float required_time,
            float success_chance,
-           std::variant<sol::function, std::function<float(const Goal)>> get_goal_change,
+           sol::function action,
+           sol::function get_goal_change,
            std::variant<sol::function, std::function<float()>> get_duration) :
         m_name(name),
         m_tags(tags),
         m_required_time(required_time),
         m_success_chance(success_chance),
+        m_action(action),
         m_get_goal_change(get_goal_change),
         m_get_duration(get_duration)
     {
-    }
-
-    Action* clone()
-    {
-        return new Action(m_name, m_tags, m_required_time, m_success_chance, m_get_goal_change, m_get_duration);
     }
 };
 inline bool operator==(const Action& lhs, const Action& rhs)
@@ -75,18 +65,6 @@ inline bool operator==(const Action& lhs, const Action& rhs)
         return false;
     }
     if (lhs.m_tags != rhs.m_tags)
-    {
-        return false;
-    }
-    if (lhs.m_action != rhs.m_action)
-    {
-        return false;
-    }
-    if (lhs.m_get_duration != rhs.m_get_duration)
-    {
-        return false;
-    }
-    if (lhs.m_get_goal_change != rhs.m_get_goal_change)
     {
         return false;
     }
