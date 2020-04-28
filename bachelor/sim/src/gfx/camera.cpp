8c140fef24d9ecfc084d22cd3f2b41ff4dbb7fab
Carl Findahl <sportleroro@gmail.com>
2020-04-28 13:41:38 +0200

    #44 Update camera view matrix to provide position more suitable for the 3D

--- bachelor/sim/src/gfx/camera.cpp
+++ bachelor/sim/src/gfx/camera.cpp
@@ -17,7 +17,7 @@ void Camera::init(glm::vec3 position)
 
 glm::mat4 Camera::get_view_matrix() const
 {
-    return glm::translate(glm::mat4(1.f), -m_pos);
+    return glm::translate(glm::mat4(1.f), m_pos);
 }
 
 glm::mat4 Camera::get_view_projection_matrix() const
