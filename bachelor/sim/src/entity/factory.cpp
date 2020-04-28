a3eed0b1b6e6f255b20feccb68a60ec3fdb2f285
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-04-28 14:48:43 +0200

    #116 Work on making action sequences and goals usable through factory

--- bachelor/sim/src/entity/factory.cpp
+++ bachelor/sim/src/entity/factory.cpp
@@ -125,13 +125,89 @@ bool spawn_tag_component(entt::entity e, entt::registry& reg, sol::table table)
 
 bool spawn_action_component(entt::entity e, entt::registry& reg, sol::table table)
 {
-    reg.assign_or_replace<component::Action>(e);
+    auto actions                       = reg.assign_or_replace<component::Action>(e);
+    const auto& action_sequence_tables = table["actions"].get_or<std::vector<sol::table>>({});
+    for (const auto& action_sequence_table : action_sequence_tables)
+    {
+        auto action_sequence = gob::Action_Sequence{
+            action_sequence_table["name"].get<std::string>(),
+            action_sequence_table["tags"].get<ETag>(),
+        };
+
+        auto current_action            = action_sequence_table["current_action"].get<sol::table>();
+        action_sequence.current_action = gob::Action{current_action["name"].get<std::string>(),
+                                                     current_action["tags"].get<ETag>(),
+                                                     current_action["required_time"].get<float>(),
+                                                     current_action["success_chance"].get<float>(),
+                                                     current_action["flags"].get<uint32_t>(),
+                                                     current_action["action"].get<sol::function>(),
+                                                     current_action["get_goal_change"].get<sol::function>()};
+        if (current_action["get_duration"].get_type() == sol::type::function)
+        {
+            action_sequence.current_action.m_get_duration = current_action["get_duration"].get<sol::function>();
+        }
+
+        for (const auto& action_table : action_sequence_table["actions"].get_or<std::vector<sol::table>>({}))
+        {
+            auto action = gob::Action{action_table["name"].get<std::string>(),
+                                      action_table["tags"].get<ETag>(),
+                                      action_table["required_time"].get<float>(),
+                                      action_table["success_chance"].get<float>(),
+                                      action_table["flags"].get<uint32_t>(),
+                                      action_table["action"].get<sol::function>(),
+                                      action_table["get_goal_change"].get<sol::function>()};
+
+            if (action_table["get_duration"].get_type() == sol::type::function)
+            {
+                action.m_get_duration = action_table["get_duration"].get<sol::function>();
+            }
+
+            action_sequence.m_actions.push_back(action);
+        }
+
+        if (action_sequence_table["run_actions"].get_type() == sol::type::function)
+        {
+            action_sequence.m_run_actions = action_sequence_table["run_actions"].get<sol::function>();
+        }
+
+        if (action_sequence_table["get_goal_change"].get_type() == sol::type::function)
+        {
+            action_sequence.m_get_goal_change = action_sequence_table["get_goal_change"].get<sol::function>();
+        }
+
+        if (action_sequence_table["get_duration"].get_type() == sol::type::function)
+        {
+            action_sequence.m_get_duration = action_sequence_table["get_duration"].get<sol::function>();
+        }
+
+        actions.actions.push_back(action_sequence);
+    }
     return true;
 }
+} // namespace detail
 
 bool spawn_goal_component(entt::entity e, entt::registry& reg, sol::table table)
 {
-    reg.assign_or_replace<component::Goal>(e, table["goals"].get_or<std::vector<sol::table>>({}));
+    auto goal_comp    = reg.assign_or_replace<component::Goal>(e);
+    const auto& goals = table["goals"].get_or<std::vector<sol::table>>({});
+    for (const auto& goal_table : goals)
+    {
+        auto goal =
+            gob::Goal{goal_table["name"].get<std::string>(), goal_table["tags"].get<ETag>(), goal_table["age"].get<float>()};
+        if (goal_table["weight_function"].get_type() == sol::type::function)
+        {
+            goal.m_weight_function = goal_table["weight_function"].get<sol::function>();
+        }
+        if (goal_table["change_over_time"].get_type() == sol::type::function)
+        {
+            goal.m_change_over_time = goal_table["change_over_time"].get<sol::function>();
+        }
+        if (goal_table["get_discontentment"].get_type() == sol::type::function)
+        {
+            goal.m_get_discontentment = goal_table["get_discontentment"].get<sol::function>();
+        }
+        goal_comp.goals.push_back(goal);
+    }
     return true;
 }
 
@@ -329,13 +405,13 @@ bool spawn_trait_component(entt::entity e, entt::registry& reg, sol::table table
     }
     return true;
 }
-} // namespace detail
 
 entt::entity spawn_entity(entt::registry& reg, sol::state_view lua, std::string_view entity, glm::vec2 position)
 {
     auto out = spawn_entity(reg, lua, entity);
 
     /** Set position if it has one */
+
     if (auto* pos = reg.try_get<component::Position>(out); pos)
     {
         pos->position = glm::vec3(position, 0.f);
