970ec6aec02f7b6b8a56fdafa61807f0ab32733b
Tomas Berger <tomashb@stud.ntnu.no>
2020-04-29 08:42:48 +0200

    #94 Made the test enviorment smaler and include names

--- bachelor/sim/res/script/scenarios/basic_needs_personality/basic_needs_personality.lua
+++ bachelor/sim/res/script/scenarios/basic_needs_personality/basic_needs_personality.lua
@@ -8,7 +8,7 @@ scenario.name = "Basic Needs Traits"
 scenario.description = "Observe as your agents walk around in a forest looking for food and water to survive. How many can be sustained?"
 
 -- The bounds of the level on the X and Y in both directions based on the center 200,200 means the total world is 400,400 with 200 units on both sides of 0,0
-scenario.bounds = Vec2:new(500.0, 500.0)
+scenario.bounds = Vec2:new(100.0, 100.0)
 
 -- Declare all systems that should be active initially
 scenario.systems = {
@@ -36,13 +36,18 @@ scenario.init = function()
     cultsim.connect("ScenarioLoaded", function(event) log.info("Scenario loaded.") end)
 
     -- Spawn 100 Deer
-    for i=1,100 do
+    for i=1,10 do
         local deer = cultsim.spawn("deer")
-
-        -- -- Randomly distribute starting needs of agents around 75, with a std deviation of 25
-        if i == 100 then
-            cultsim.add_romance(99, 99, 30)
+        -- Set Random name of deer
+        local name = cultsim.get_component(deer, component.name)
+        if(cultsim.get_component(deer, component.reproduction).sex == 1) then
+            local genname = cultsim.generate_name("germanic", true)
+            name.name = string.format("%s %s", genname.first, genname.last)
+        else 
+            local genname = cultsim.generate_name("germanic", false)
+            name.name = string.format("%s %s", genname.first, genname.last)
         end
+        -- Randomly distribute starting needs of agents around 75, with a std deviation of 25
         local need_comp = cultsim.get_component(deer, component.need)
         for i, need in ipairs(need_comp.required_needs) do
             need.status = random:normal(75.0, 25.0)
@@ -50,13 +55,13 @@ scenario.init = function()
     end
 
     -- Spawn 75 Food Sources and Water Sources
-    for i=1,75 do
+    for i=1,7 do
         cultsim.spawn("food_source")
         cultsim.spawn("water_source")
     end
 
     -- Create some food / water initially
-    for i=1,50 do
+    for i=1,5 do
         cultsim.spawn_at("water", Vec2:new(random:normal(0.0, 70.0), random:normal(0.0, 70.0)))
         cultsim.spawn_at("food", Vec2:new(random:normal(0.0, 70.0), random:normal(0.0, 70.0)))
     end
