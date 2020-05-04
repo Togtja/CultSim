fa870f0c7bee77f3149f2bbad23794baf668bca8
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-05-04 12:39:17 +0200

    #44 Changed assignment style to squiggly brackets and added todos

--- bachelor/sim/src/entity/components/components.h
+++ bachelor/sim/src/entity/components/components.h
@@ -20,6 +20,10 @@
 
 namespace cs::component
 {
+/** TODO: Add documentation for each component */
+
+using Delete = entt::tag<"Delete"_hs>;
+
 struct Position
 {
     glm::vec3 position{};
@@ -29,13 +33,14 @@ struct Movement
 {
     std::vector<glm::vec3> desired_position{};
     glm::vec2 direction{};
-    float speed        = 1.f;
-    float avoidance_cd = 0.f;
+    float speed{1.f};
+    float avoidance_cd{};
     int avoid_count{};
 
-    float speed_multi = 1.f;
+    float speed_multi{1.f};
 };
 
+/** TODO: Check if this needs renaming */
 struct Meta
 {
     std::string name{};
@@ -49,7 +54,7 @@ struct Sprite
 
 struct Animation
 {
-    float time_per_frame = 1.f / 4.f;
+    float time_per_frame{1.f / 4.f};
     float current_progress{};
 };
 
@@ -79,13 +84,13 @@ struct Hearing
 
 struct Timer
 {
-    using OnCompleteFunction = std::function<void(entt::entity, entt::registry&)>;
+    using OnCompleteFunction = std::variant<std::function<void(entt::entity, entt::registry&)>, sol::function>;
 
     float time_to_complete{};
     float time_spent{};
 
     int number_of_loops{};
-    std::variant<OnCompleteFunction, sol::function> on_complete{};
+    OnCompleteFunction on_complete{};
 };
 
 struct Health
@@ -106,17 +111,6 @@ struct Age
     float current_age{};
 
     float average_life_expectancy{};
-};
-
-struct DropItems
-{
-    enum EDeathDrops
-    {
-        Meat,
-        Wood
-    };
-
-    std::vector<EDeathDrops> deathdrops{};
 };
 
 struct Strategy
@@ -183,14 +177,11 @@ struct Memory
     float max_retention_time{};
 
     std::vector<memory::Container> memory_container{};
+
     Memory(Memory&&) = default;
     Memory& operator=(Memory&&) = default;
     Memory(const Memory&)       = default;
     Memory& operator=(const Memory&) = default;
-};
-
-struct Delete
-{
 };
 
 struct Reproduction
@@ -201,27 +192,28 @@ struct Reproduction
         Female = false
     };
 
-    ESex sex = Male;
+    ESex sex{Male};
     uint16_t number_of_children{};
 
-    bool lays_eggs = false;
+    bool lays_eggs{false};
     std::string egg_type;
 
-    ESex incubator = Female;
+    ESex incubator{Female};
 
     /** If true then users says they has specified start/peak/end fertility */
-    bool has_fertility = false;
+    bool has_fertility{false};
+
     /** Needs age component to work */
     float start_fertility{};
     float peak_fertility{};
     float end_fertility{};
 
     /** General Pregnancy stat */
-    int mean_children_pp = 1;
+    int mean_children_pp{1};
     float children_deviation{};
 
     /** Static fertility */
-    float fertility = 1.f;
+    float fertility{1.f};
 
     float average_gestation_period{};
     float gestation_deviation{};
@@ -233,17 +225,20 @@ struct Name
     std::string name{};
 };
 
+/** TODO: Make stuff into struct to differentiate between mother and father */
 struct Pregnancy
 {
+    using ParentsPair = std::pair<entt::entity, entt::entity>;
+
     float time_since_start{};
     float gestation_period{};
 
     /** First in the incubator, then second is other parent (In Humans: first is Mom, second is Dad) */
-    std::pair<entt::entity, entt::entity> parents = {entt::null, entt::null};
+    ParentsPair parents{entt::null, entt::null};
 
     uint16_t children_in_pregnancy{};
 
-    bool is_egg = false;
+    bool is_egg{false};
 };
 
 struct Goal
@@ -253,13 +248,13 @@ struct Goal
 
 struct Action
 {
-    cs::gob::Action_Sequence current_action_sequence{};
-    std::vector<cs::gob::Action_Sequence> actions{};
+    cs::gob::ActionSequence current_action_sequence{};
+    std::vector<cs::gob::ActionSequence> actions{};
 };
 
 struct Sphere3D
 {
-    float radius = 1.f;
+    float radius{1.f};
 };
 
 namespace detail
@@ -270,11 +265,11 @@ struct Trait
     std::string name;
     std::string desc;
 
-    bool can_inherit     = false;
-    float inherit_chance = 1.0f;
+    bool can_inherit{false};
+    float inherit_chance{1.0f};
 
-    bool can_mutate       = false;
-    float mutation_chance = 0.0001f;
+    bool can_mutate{false};
+    float mutation_chance{0.0001f};
 
     sol::function attain;
     sol::function lose;
@@ -294,8 +289,10 @@ struct Traits
 {
     /** The trait a species/aganets start with when running a simulation */
     std::vector<detail::Trait> start_traits;
+
     /** List of possible atainable traits */
     std::vector<detail::Trait> attainable_traits;
+
     /** The traits that the agent currently has */
     std::vector<detail::Trait> acquired_traits;
 };
@@ -303,27 +300,23 @@ struct Traits
 struct Relationship
 {
     /** Used to add a new entry in the relatip table */
-    bool new_create         = true;
-    uint8_t default_friend  = 0;
-    uint8_t default_romance = 0;
+    bool new_create{true};
+    uint8_t default_friend{0};
+    uint8_t default_romance{0};
 
-    uint8_t self_friend  = 0;
-    uint8_t self_romance = 0;
+    uint8_t self_friend{0};
+    uint8_t self_romance{0};
 
     /** Below used for family tree: */
 
     /** Mother Ids for Global and Relationship Ids */
-    system::ParentEntityIds mom = {entt::null, entt::null};
+    system::ParentEntityIds mom{entt::null, entt::null};
 
     /** Father Ids for Global and Relationship Ids */
-    system::ParentEntityIds dad = {entt::null, entt::null};
+    system::ParentEntityIds dad{entt::null, entt::null};
 
     /** old_id is entt id for context registry */
-    entt::entity old_id = entt::null;
-};
-
-struct AI
-{
+    entt::entity old_id{entt::null};
 };
 
 /** Component for use in Lua */
