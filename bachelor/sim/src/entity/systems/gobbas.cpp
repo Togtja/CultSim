b5425ecfd0f527cbc8bce8c6e8b9e8f6fefe5002
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-05-04 14:17:40 +0200

    #44 Add documentation request to gobbas

--- bachelor/sim/src/entity/systems/gobbas.cpp
+++ bachelor/sim/src/entity/systems/gobbas.cpp
@@ -8,14 +8,12 @@ void GOBBAS::update(float dt)
 {
     CS_AUTOTIMER(Goal Oriented Action System);
 
-    auto& registry = *m_context.registry;
-
-    auto view = registry.view<component::Action>();
+    auto view = m_context.registry->view<component::Action>();
     view.each([](entt::entity e, component::Action& action) {
         bool finished{};
         std::string error{};
 
-        /**While we are not finished or get an error, keep working*/
+        /** While we are not finished or get an error, keep working */
         if (action.current_action_sequence.m_run_actions.index() == 0)
         {
             finished = std::get<sol::function>(action.current_action_sequence.m_run_actions)(e, error).get<bool>();
@@ -26,11 +24,14 @@ void GOBBAS::update(float dt)
                 action.current_action_sequence.m_run_actions)(action.current_action_sequence, e, &error);
         }
 
-        /**If we get an error or finish, clear our current action sequence*/
-        if (error != "" || finished)
+        /** If we get an error or finish, clear our current action sequence */
+        if (finished)
+        {
+            action.current_action_sequence = {};
+        }
+        if (error != "")
         {
             spdlog::get("agent")->error(error);
-            action.current_action_sequence = {};
         }
     });
 }
--- bachelor/sim/src/entity/systems/gobbas.h
+++ bachelor/sim/src/entity/systems/gobbas.h
@@ -3,10 +3,12 @@
 
 namespace cs::system
 {
+/** TODO: Documentation */
 class GOBBAS : public ISystem
 {
 public:
     using ISystem::ISystem;
+
     void update(float dt) override;
 
     ISystem* clone() override;
