35f4786efab4b20cadc0f02bc303e03cece999fb
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-05-04 12:53:54 +0200

    #44 Make != operator the opposite of the == operator

--- bachelor/sim/src/entity/components/need.h
+++ bachelor/sim/src/entity/components/need.h
@@ -9,6 +9,7 @@ namespace cs::ai
 {
 struct Need
 {
+    using WeightFunction = std::variant<sol::function, std::function<float(const Need&)>>;
     std::string name{};
 
     float weight{};
@@ -21,13 +22,13 @@ struct Need
 
     ETag tags{};
 
-    std::variant<sol::function, std::function<float(const Need&)>> weight_func = [](const ai::Need& local) {
+    WeightFunction weight_func = [](const ai::Need& local) {
         return (100 / (local.status + 1)) * local.weight * local.weight_multi;
     };
 
     // Is only used with traits
-    float weight_multi = 1.f;
-    float decay_multi  = 1.f;
+    float weight_multi{1.f};
+    float decay_multi{1.f};
 };
 
 inline bool operator<(Need const& lhs, Need const& rhs)
@@ -85,6 +86,6 @@ inline bool operator==(Need const& lhs, Need const& rhs)
 
 inline bool operator!=(Need const& lhs, Need const& rhs)
 {
-    return lhs.name != rhs.name;
+    return !(lhs == rhs);
 }
 } // namespace cs::ai
