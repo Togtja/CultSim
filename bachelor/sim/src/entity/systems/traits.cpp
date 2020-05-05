334889c1597a1e7f3b90bf6bce7c8b4a9c94bdb7
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-05-05 11:54:44 +0200

    #44 Fix naming and commenting styles, add request for documentation

--- bachelor/sim/src/entity/systems/traits.cpp
+++ bachelor/sim/src/entity/systems/traits.cpp
@@ -9,37 +9,37 @@ void Traits::update(float dt)
     CS_AUTOTIMER(Traits System);
     auto view = m_context.registry->view<component::Traits>();
     view.each([this](entt::entity e, component::Traits& traits) {
-        // Trying to get traits
-        for (auto&& i : traits.attainable_traits)
+        /** Trying to get traits */
+        for (const auto& trait : traits.attainable_traits)
         {
-            // As long as the attainable trait is not already required
-            const auto& it = std::find(traits.acquired_traits.begin(), traits.acquired_traits.end(), i);
+            /** As long as the attainable trait is not already acquired */
+            const auto& it = std::find(traits.acquired_traits.begin(), traits.acquired_traits.end(), trait);
             if (it == traits.acquired_traits.end())
             {
-                // Check if we satisfy the user typed requirement
-                if (i.attain(e))
+                /* Check if we satisfy the user typed requirement */
+                if (trait.attain(e))
                 {
-                    i.affect(e);
-                    traits.acquired_traits.push_back(i);
+                    trait.affect(e);
+                    traits.acquired_traits.push_back(trait);
                 }
             }
         }
 
+        /* Add traits to be deleted (to avoid deleting in loop) */
         std::vector<component::detail::Trait> to_delete;
-        // Add traits to be deleted (to avoid deleting in loop)
-        for (auto&& i : traits.acquired_traits)
+        for (const auto& trait : traits.acquired_traits)
         {
-            if (i.lose(e))
+            if (trait.lose(e))
             {
-                i.remove_affect(e);
-                to_delete.push_back(i);
+                trait.remove_affect(e);
+                to_delete.push_back(trait);
             }
         }
 
-        // Delete the traits we found
-        for (auto&& i : to_delete)
+        /* Delete the traits we found */
+        for (const auto& trait : to_delete)
         {
-            const auto& it = std::find(traits.acquired_traits.begin(), traits.acquired_traits.end(), i);
+            const auto& it = std::find(traits.acquired_traits.begin(), traits.acquired_traits.end(), trait);
             if (it != traits.acquired_traits.end())
             {
                 traits.acquired_traits.erase(it);
