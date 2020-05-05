6a3855d434c62e5324eb944acdbebf2635e54d4a
Leon Cinquemani <leonnc@stud.ntnu.no>
2020-05-05 08:45:51 +0200

    #44 Refactor Need.cpp by fixing commenting style and formatting

--- bachelor/sim/src/entity/systems/need.cpp
+++ bachelor/sim/src/entity/systems/need.cpp
@@ -15,34 +15,36 @@ void Need::update(float dt)
     view.each([dt, this](entt::entity e, component::Need& needs) {
         for (auto& need : needs.needs)
         {
-            // Ensure statuses cannot go below 0% or above 100%
+            /** Ensure statuses cannot go below 0% or above 100% */
             need.status = std::clamp(need.status - (need.decay_rate * need.decay_multi * dt), 0.f, 100.f);
 
             auto found = std::find(needs.vital_needs.begin(), needs.vital_needs.end(), need);
-            // If the need does exist in pressing needs
+            /** If the need does exist in pressing needs */
             if (found != needs.vital_needs.end())
             {
                 found->status = need.status;
-                // Remove it if it should not be there
+                /** Remove it if it should not be there */
                 if (found->status > 50.f)
                 {
                     needs.vital_needs.erase(found);
                     m_context.dispatcher->enqueue<event::NeedNoLongerCritical>(
                         event::NeedNoLongerCritical{e, need.tags, need.status, need.weight});
                 }
-                // If there are no vital needs left and we are switching to Leisure Needs
+
+                /** If there are no vital needs left and we are switching to Leisure Needs */
                 if (needs.vital_needs.empty())
                 {
                     m_context.dispatcher->enqueue<event::SwitchNeedContext>(event::SwitchNeedContext{e});
                 }
             }
-            // If the need does not exist in pressing needs
+            /** If the need does not exist in pressing needs */
             else
             {
-                // Add it if it should be there
+                /** Add it if it should be there */
+                /** TODO: Make needs have a threshhold variable the user can set */
                 if (need.status <= 50.f)
                 {
-                    // If vital needs are empty before adding this one
+                    /** If vital needs are empty before adding this one */
                     if (needs.vital_needs.empty())
                     {
                         m_context.dispatcher->enqueue<event::SwitchNeedContext>(event::SwitchNeedContext{e});
