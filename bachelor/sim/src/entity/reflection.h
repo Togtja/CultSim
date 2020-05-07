846151515b6dba80773fadc1ae88ce60e988cb12
Carl Findahl <sportleroro@gmail.com>
2020-05-07 13:16:49 +0200

    #44 Comment on reflection.h$

--- bachelor/sim/src/entity/reflection.h
+++ bachelor/sim/src/entity/reflection.h
@@ -6,8 +6,13 @@
 
 namespace cs::meta
 {
-/** TODO: Documentation */
+/**
+ * Reflect pure data types so they are available for later
+ */
 void reflect_data_types();
 
+/**
+ * Reflect the system types for use with spawning from Lua
+ */
 void reflect_systems();
 } // namespace cs::meta
