be75fa2c2b2a0f3303cf7ebb5862ac51d581cbdc
Carl Findahl <sportleroro@gmail.com>
2020-05-07 14:44:25 +0200

    #44 Rename and document load scenario scene

--- bachelor/sim/src/scenes/loadscenario_scene.h
+++ bachelor/sim/src/scenes/loadscenario_scene.h
@@ -2,15 +2,18 @@
 
 #include "scene.h"
 
-/** TODO: Rename loadsenario_scene.h to loadscenario_scene.h */
 namespace cs
 {
-/** TODO: Documentation */
+/**
+ * The load scenario scene provides the user with options before running a scenario
+ */
 class LoadScenarioScene : public IScene
 {
 private:
+    /** Max length of the seed */
     static inline constexpr unsigned s_seed_length = 32;
 
+    /** The seed in a string / human readable format */
     char m_seed[s_seed_length];
 
 public:
