d826f9388837657ddfa9c56a19b8f2bccc28d6de
Carl Findahl <sportleroro@gmail.com>
2020-05-07 11:45:22 +0200

    #44 Clarify variables in autotimer.cpp

--- bachelor/sim/src/debug/auto_timer.cpp
+++ bachelor/sim/src/debug/auto_timer.cpp
@@ -10,8 +10,10 @@
 
 namespace cs
 {
-/** TODO: Better variable name */
-static constexpr std::array<glm::vec4, 2> s_ms_col{glm::vec4{0.2f, 1.f, 0.66f, 1.f}, glm::vec4{1.f, 0.4f, 0.2f, 1.f}};
+/** Variables for coloring the text output */
+static constexpr auto s_color_good_time = glm::vec4{0.2f, 1.f, 0.66f, 1.f};
+static constexpr auto s_color_bad_time  = glm::vec4{1.f, 0.4f, 0.2f, 1.f};
+static constexpr std::array<glm::vec4, 2> s_time_color_coding{s_color_good_time, s_color_bad_time};
 
 void AutoTimer::show_debug_ui()
 {
@@ -27,7 +29,11 @@ void AutoTimer::show_debug_ui()
             ImGui::TableNextCell();
             ImGui::Text("%s", k.name.data());
             ImGui::TableNextCell();
-            ImGui::TextColored(glm::mix(s_ms_col[0], s_ms_col[1], glm::vec4(k.time_ms / 16.f)), "%.3lf", k.time_ms);
+
+            /** Show time in MS colored from green to red based on how long it took relative to 60 FPS */
+            ImGui::TextColored(glm::mix(s_time_color_coding[0], s_time_color_coding[1], glm::vec4(k.time_ms / 16.f)),
+                               "%.3lf",
+                               k.time_ms);
         }
         ImGui::EndTable();
     }
