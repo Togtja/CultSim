818a95a5d4e67e5df97896d961e8bae4d36b225b
Tomas Berger <tomashb@stud.ntnu.no>
2020-05-19 08:46:53 +0200

    #118 Add agent with their componenet

--- bachelor/sim/res/script/scenarios/RL_AI/entities/r_learners.lua
+++ bachelor/sim/res/script/scenarios/RL_AI/entities/r_learners.lua
@@ -1,3 +1,4 @@
+
 entity = {
     PositionComponent = {
         position = Vec2:new(random:uniform(-scenario.bounds.x, scenario.bounds.x),
@@ -16,8 +17,39 @@ entity = {
         radius = 40.0,
         fov = 0 -- 0 means no FOV
     },
-    NameComponent = 
-    {
-     entity_type = "Deer"
+    NameComponent =  {
+     entity_type = "AI Learners"
+    },
+    LuaComponent = {
+        lua_id = 1,
+        speed = 50,
+        direction = Vec2.new(0, 0),
+
+        reach_goal = true,
+
+        q_brain = ai.QBrain(),
+
+        action = {
+            ac = ai_actions.nothing.value,
+            loc = Vec2.new(0,0),
+            entity = nil,
+            type = nil
+        },
+
+        state = ai_states.nothing.value,
+        pre_state = ai_states.nothing.value
+
+    },
+    TagComponent = {
+        tags = ETag.Avoidable
+    },
+    HealthComponent = {
+        health = 100.0,
+        tickdown_rate = 1.0,
+        vital_needs = ETag.Food | ETag.Drink
+    },
+    NeedComponent = {
+        -- Corresponds to entires in the needs.lua file
+        required_needs = { needs.hunger, needs.thirst}
     },
 }
